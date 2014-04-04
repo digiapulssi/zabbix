digiapulssi/zabbix

commit 1fc62367eba386f56759ca9af54fd74921435142

........S. [ZBX-7856] replaced open_proc_file() with fopen()
git-svn-id: svn://svn.zabbix.com/branches/dev@44033 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

ZBX-7856/src/libs/zbxsysinfo/linux/proc.c
@@ -25,16 +25,6 @@
 #define DO_MIN 2
 #define DO_AVG 3
 
-static FILE	*open_proc_file(const char *filename)
-{
-	zbx_stat_t	s;
-
-	if (0 != zbx_stat(filename, &s))
-		return NULL;
-
-	return fopen(filename, "r");
-}
-
 static int	get_cmdline(FILE *f_cmd, char **line, size_t *line_offset)
 {
 	size_t	line_alloc = ZBX_KIBIBYTE, n;
@@ -277,12 +267,12 @@ int	PROC_MEM(const char *cmd, const char *param, unsigned flags, AGENT_RESULT *r
 
 		zbx_snprintf(tmp, sizeof(tmp), "/proc/%s/cmdline", entries->d_name);
 
-		if (NULL == (f_cmd = open_proc_file(tmp)))
+		if (NULL == (f_cmd = fopen(tmp)))
 			continue;
 
 		zbx_snprintf(tmp, sizeof(tmp), "/proc/%s/status", entries->d_name);
 
-		if (NULL == (f_stat = open_proc_file(tmp)))
+		if (NULL == (f_stat = fopen(tmp)))
 			continue;
 
 		if (FAIL == check_procname(f_cmd, f_stat, procname))
@@ -416,12 +406,12 @@ int	PROC_NUM(const char *cmd, const char *param, unsigned flags, AGENT_RESULT *r
 
 		zbx_snprintf(tmp, sizeof(tmp), "/proc/%s/cmdline", entries->d_name);
 
-		if (NULL == (f_cmd = open_proc_file(tmp)))
+		if (NULL == (f_cmd = fopen(tmp)))
 			continue;
 
 		zbx_snprintf(tmp, sizeof(tmp), "/proc/%s/status", entries->d_name);
 
-		if (NULL == (f_stat = open_proc_file(tmp)))
+		if (NULL == (f_stat = fopen(tmp)))
 			continue;
 
 		if (FAIL == check_procname(f_cmd, f_stat, procname))

commit 5f87c4ac0c6577335519b97b772b05f6f5896749

...G...... [ZBX-7856] fixed compilation problem introduced in r44033
git-svn-id: svn://svn.zabbix.com/branches/dev@44061 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

ZBX-7856/src/libs/zbxsysinfo/linux/proc.c
@@ -267,12 +267,12 @@ int	PROC_MEM(const char *cmd, const char *param, unsigned flags, AGENT_RESULT *r
 
 		zbx_snprintf(tmp, sizeof(tmp), "/proc/%s/cmdline", entries->d_name);
 
-		if (NULL == (f_cmd = fopen(tmp)))
+		if (NULL == (f_cmd = fopen(tmp, "r")))
 			continue;
 
 		zbx_snprintf(tmp, sizeof(tmp), "/proc/%s/status", entries->d_name);
 
-		if (NULL == (f_stat = fopen(tmp)))
+		if (NULL == (f_stat = fopen(tmp, "r")))
 			continue;
 
 		if (FAIL == check_procname(f_cmd, f_stat, procname))
@@ -406,12 +406,12 @@ int	PROC_NUM(const char *cmd, const char *param, unsigned flags, AGENT_RESULT *r
 
 		zbx_snprintf(tmp, sizeof(tmp), "/proc/%s/cmdline", entries->d_name);
 
-		if (NULL == (f_cmd = fopen(tmp)))
+		if (NULL == (f_cmd = fopen(tmp, "r")))
 			continue;
 
 		zbx_snprintf(tmp, sizeof(tmp), "/proc/%s/status", entries->d_name);
 
-		if (NULL == (f_stat = fopen(tmp)))
+		if (NULL == (f_stat = fopen(tmp, "r")))
 			continue;
 
 		if (FAIL == check_procname(f_cmd, f_stat, procname))