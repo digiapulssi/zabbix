9754385a312a83e05a71e28f7fadd3c6f3c3d790
andris <andris@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2017-03-03 15:49:04 +0000

    ...G...... [ZBX-11671] renamed constants ZBX_PRINT_xx to ZBX_FATAL_LOG_xx
    git-svn-id: svn://svn.zabbix.com/branches/dev@66141 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-11671/src/libs/zbxnix/fatal.h
+++ ZBX-11671/src/libs/zbxnix/fatal.h
@@ -22,10 +22,10 @@
 
 #include <signal.h>
 
-#define ZBX_PRINT_PC_REG_SF		0x0001
-#define ZBX_PRINT_BACKTRACE		0x0002
-#define ZBX_PRINT_MEM_MAP		0x0004
-#define ZBX_PRINT_FULL_INFO		(ZBX_PRINT_PC_REG_SF | ZBX_PRINT_BACKTRACE | ZBX_PRINT_MEM_MAP)
+#define ZBX_FATAL_LOG_PC_REG_SF		0x0001
+#define ZBX_FATAL_LOG_BACKTRACE		0x0002
+#define ZBX_FATAL_LOG_MEM_MAP		0x0004
+#define ZBX_FATAL_LOG_FULL_INFO		(ZBX_FATAL_LOG_PC_REG_SF | ZBX_FATAL_LOG_BACKTRACE | ZBX_FATAL_LOG_MEM_MAP)
 
 const char	*get_signal_name(int sig);
 void	print_fatal_info(void *context, zbx_uint64_t print_flag);
