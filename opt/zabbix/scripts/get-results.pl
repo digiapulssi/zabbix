78d13b3cfa231f2d56183612a804329eb42eeeb7
dimir <dimir@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2017-03-15 11:02:31 +0000

    ....I..... [RSM-1] get-results.pl: update helper script
    git-svn-id: svn://svn.zabbix.com/branches/dev/rsm-dev@66406 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- opt/zabbix/scripts/get-results.pl
+++ opt/zabbix/scripts/get-results.pl
@@ -8,14 +8,13 @@ use lib $MYDIR;
 
 use strict;
 use warnings;
-
 use RSM;
-use RSMSLV;
+use RSMSLV1;
 
 parse_opts('tld=s', 'from=n', 'till=n', 'service=s');
 
 # do not write any logs
-setopt('test');
+setopt('nolog');
 
 if (opt('debug'))
 {
@@ -33,90 +32,100 @@ set_slv_config(get_rsm_config());
 
 db_connect();
 
-my ($key, $cfg_max_value, $service_type, $delay, $eomonth);
+my ($key, $service_name, $service_option, $delay, $proto, $command);	# $proto is needed for DNS, $command for EPP
 
-my $from = getopt('from');
-my $till = getopt('till');
-my $value_ts = $till;
+my ($from, $till);
 
-# calculate estimated total number of tests this month unless at least one time bound specified
-my $calculate_month_total = ((defined($from) or defined($till)) ? 0 : 1);
-
-unless (defined($from) and defined($till))
+if (opt('from'))
 {
-	my @bounds = get_curmon_bounds();
-
-	$from = $bounds[0] unless (defined($from));
-	$till = $bounds[1] unless (defined($till));
-	$value_ts = $till;
-	$eomonth = $bounds[2];
+	$from = truncate_from(getopt('from'));
+}
+if (opt('till'))
+{
+	$till = truncate_till(getopt('till'));
 }
 
 if (getopt('service') eq 'tcp-dns-rtt')
 {
 	$key = 'rsm.dns.tcp.rtt[{$RSM.TLD},';
-	$cfg_max_value = get_macro_dns_tcp_rtt_low();
-	$delay = get_macro_dns_tcp_delay() if ($calculate_month_total != 0);
-	$service_type = 'DNS';
+	$delay = get_macro_dns_tcp_delay();
+	$service_name = 'DNS';
+	$service_option = ENABLED_DNS;
+	$proto = PROTO_TCP;
 }
 elsif (getopt('service') eq 'udp-dns-rtt')
 {
 	$key = 'rsm.dns.udp.rtt[{$RSM.TLD},';
-	$cfg_max_value = get_macro_dns_udp_rtt_low();
-	$delay = get_macro_dns_udp_delay() if ($calculate_month_total != 0);
-	$service_type = 'DNS';
+	$delay = get_macro_dns_udp_delay();
+	$service_name = 'DNS';
+	$service_option = ENABLED_DNS;
+	$proto = PROTO_UDP;
 }
 elsif (getopt('service') eq 'dns-upd')
 {
 	$key = 'rsm.dns.udp.upd[{$RSM.TLD},';
-	$cfg_max_value = get_macro_dns_update_time();
-	$delay = get_macro_dns_udp_delay() if ($calculate_month_total != 0);
-	$service_type = 'EPP';
+	$delay = get_macro_dns_udp_delay();
+	$service_name = 'EPP';
+	$service_option = ENABLED_EPP;
 }
 elsif (getopt('service') eq 'rdds43-rtt')
 {
 	$key = 'rsm.rdds.43.rtt[{$RSM.TLD}]';
-	$cfg_max_value = get_macro_rdds_rtt_low();
-	$delay = get_macro_rdds_delay() if ($calculate_month_total != 0);
-	$service_type = 'RDDS';
+	$delay = get_macro_rdds_delay();
+	$service_name = 'RDDS';
+	$service_option = ENABLED_RDDS43;
 }
 elsif (getopt('service') eq 'rdds80-rtt')
 {
 	$key = 'rsm.rdds.80.rtt[{$RSM.TLD}]';
-	$cfg_max_value = get_macro_rdds_rtt_low();
-	$delay = get_macro_rdds_delay() if ($calculate_month_total != 0);
-	$service_type = 'RDDS';
+	$delay = get_macro_rdds_delay();
+	$service_name = 'RDDS';
+	$service_option = ENABLED_RDDS80;
 }
-elsif (getopt('service') eq 'rdds-upd')
+elsif (getopt('service') eq 'rdap-rtt')
+{
+	$key = 'rsm.rdds.rdap.rtt[{$RSM.TLD}]';
+	$delay = get_macro_rdds_delay();
+	$service_name = 'RDDS';
+	$service_option = ENABLED_RDAP;
+}
+elsif (getopt('service') eq 'rdds43-upd')
 {
 	$key = 'rsm.rdds.43.upd[{$RSM.TLD}]';
-	$cfg_max_value = get_macro_rdds_rtt_low();
-	$delay = get_macro_rdds_delay() if ($calculate_month_total != 0);
-	$service_type = 'EPP';
+	$delay = get_macro_rdds_delay();
+	$service_name = 'RDDS';
+	$service_option = ENABLED_RDDS43_EPP;
+}
+elsif (getopt('service') eq 'rdap-upd')
+{
+	$key = 'rsm.rdds.rdap.upd[{$RSM.TLD}]';
+	$delay = get_macro_rdds_delay();
+	$service_name = 'RDDS';
+	$service_option = ENABLED_RDAP_EPP;
 }
 elsif (getopt('service') eq 'epp-login-rtt')
 {
-	my $command = 'login';
+	$command = 'login';
 	$key = 'rsm.epp.rtt[{$RSM.TLD},' . $command . ']';
-	$cfg_max_value = get_macro_epp_rtt_low($command);
-	$delay = get_macro_epp_delay() if ($calculate_month_total != 0);
-	$service_type = 'EPP';
+	$delay = get_macro_epp_delay();
+	$service_name = 'EPP';
+	$service_option = ENABLED_EPP;
 }
 elsif (getopt('service') eq 'epp-info-rtt')
 {
-	my $command = 'info';
+	$command = 'info';
 	$key = 'rsm.epp.rtt[{$RSM.TLD},' . $command . ']';
-	$cfg_max_value = get_macro_epp_rtt_low($command);
-	$delay = get_macro_epp_delay() if ($calculate_month_total != 0);
-	$service_type = 'EPP';
+	$delay = get_macro_epp_delay();
+	$service_name = 'EPP';
+	$service_option = ENABLED_EPP;
 }
 elsif (getopt('service') eq 'epp-update-rtt')
 {
-	my $command = 'update';
+	$command = 'update';
 	$key = 'rsm.epp.rtt[{$RSM.TLD},' . $command . ']';
-	$cfg_max_value = get_macro_epp_rtt_low($command);
-	$delay = get_macro_epp_delay() if ($calculate_month_total != 0);
-	$service_type = 'EPP';
+	$delay = get_macro_epp_delay();
+	$service_name = 'EPP';
+	$service_option = ENABLED_EPP;
 }
 else
 {
@@ -124,34 +133,44 @@ else
 	usage(2);
 }
 
-my $probe_avail_limit = get_macro_probe_avail_limit();
-my $probes_ref = get_probes($service_type);
-my $probe_times_ref = get_probe_times($from, $till, $probe_avail_limit, $probes_ref);
-my $tlds_ref = opt('tld') ? [ getopt('tld') ] : get_tlds($service_type);
+($from, $till) = get_default_period(time() - $delay - AVAIL_SHIFT_BACK, $delay, getopt('from'), getopt('till'));
+
+info('selected period: ', selected_period($from, $till));
+
+my $probes_ref = get_probes($service_option);
+my $probe_times_ref = get_probe_times($from, $till, $probes_ref);
+
+my $tlds_ref = opt('tld') ? [ getopt('tld') ] : get_tlds($service_option);
+
+my $rtt_low;	# used in __check_test()
 
 foreach (@$tlds_ref)
 {
 	$tld = $_;
 
-	my $items_ref;
+	my ($items_ref);
 
 	if ("," eq substr($key, -1))
 	{
 		my $nsips_ref = get_templated_nsips($tld, $key);
-		$items_ref = get_all_ns_items($nsips_ref, $key, $tld);
+		$items_ref = __get_all_ns_items($nsips_ref, $key, $tld);
 	}
 	else
 	{
 		$items_ref = get_all_items($key, $tld);
 	}
 
-	my $result = get_results($tld, $value_ts, $probe_times_ref, $items_ref, \&check_item_value);
+	# used in __check_test()
+	$rtt_low = get_rtt_low(lc($service_name), $proto, $command);
+
+	my $result = get_results($tld, $probe_times_ref, $items_ref, \&__check_test);
 
 	foreach my $nsip (keys(%$result))
 	{
 		my $total = $result->{$nsip}->{'total'};
 		my $successful = $result->{$nsip}->{'successful'};
 		my $nsip_label = '';
+
 		$nsip_label = "$nsip: " unless ($nsip eq '');
 
 		if ($total == 0)
@@ -161,12 +180,6 @@ foreach (@$tlds_ref)
 		}
 
 		info($nsip_label, "$successful/$total successful results from ", ts_str($from), " ($from) till ", ts_str($till), " ($till)");
-
-		if ($calculate_month_total != 0)
-		{
-			my $month_total = $total + int(($eomonth - $value_ts) / $delay);
-			info($nsip_label, "month total $month_total tests");
-		}
 	}
 }
 
@@ -175,11 +188,41 @@ $tld = undef;
 
 slv_exit(SUCCESS);
 
-sub check_item_value
+sub __check_test
 {
 	my $value = shift;
 
-	return (is_service_error($value) == SUCCESS or $value > $cfg_max_value) ? E_FAIL : SUCCESS;
+	return (is_service_error($value) == SUCCESS or $value > $rtt_low) ? E_FAIL : SUCCESS;
+}
+
+sub __get_all_ns_items
+{
+	my $nss_ref = shift; # array reference of name servers ("name,IP")
+	my $cfg_key_in = shift;
+	my $tld = shift;
+
+	my @keys;
+	push(@keys, "'" . $cfg_key_in . $_ . "]'") foreach (@$nss_ref);
+
+	my $keys_str = join(',', @keys);
+
+	my $rows_ref = db_select(
+		"select h.hostid,i.itemid,i.key_,h.host ".
+		"from items i,hosts h ".
+		"where i.hostid=h.hostid".
+			" and h.host like '$tld %'".
+			" and i.templateid is not null".
+			" and i.key_ in ($keys_str)");
+
+	my %all_ns_items;
+	foreach my $row_ref (@$rows_ref)
+	{
+		$all_ns_items{$row_ref->[0]}{$row_ref->[1]} = get_nsip_from_key($row_ref->[2]);
+	}
+
+	fail("cannot find items ($keys_str) at host ($tld *)") if (scalar(keys(%all_ns_items)) == 0);
+
+	return \%all_ns_items;
 }
 
 __END__
@@ -190,7 +233,7 @@ get-results.pl - get successful/total test results of the service for given peri
 
 =head1 SYNOPSIS
 
-get-results.pl --service <tcp-dns-rtt|udp-dns-rtt|dns-upd|rdds43-rtt|rdds80-rtt|rdds-upd|epp-login-rtt|epp-info-rtt|epp-update-rtt> [--tld tld] [--from timestamp] [--till timestamp] [--debug] [--help]
+get-results.pl --service <tcp-dns-rtt|udp-dns-rtt|dns-upd|rdds43-rtt|rdds80-rtt|rdds43-upd|epp-login-rtt|epp-info-rtt|epp-update-rtt> [--tld tld] [--from timestamp] [--till timestamp] [--debug] [--help]
 
 =head1 OPTIONS
 
@@ -198,7 +241,7 @@ get-results.pl --service <tcp-dns-rtt|udp-dns-rtt|dns-upd|rdds43-rtt|rdds80-rtt|
 
 =item B<--service> name
 
-Specify the name of the service. Supported services: tcp-dns-rtt, udp-dns-rtt, dns-upd, rdds43-rtt, rdds80-rtt, rdds-upd, epp-login-rtt, epp-info-rtt, epp-update-rtt.
+Specify the name of the service. Supported services: tcp-dns-rtt, udp-dns-rtt, dns-upd, rdds43-rtt, rdds80-rtt, rdds43-upd, epp-login-rtt, epp-info-rtt, epp-update-rtt.
 
 =item B<--tld> tld
 
