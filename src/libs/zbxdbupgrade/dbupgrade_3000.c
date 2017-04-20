29cb992cdf62b2ad9f454bf63451945d02b5c3fb
dimir <dimir@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2017-04-20 13:03:14 +0000

    ....I..... [RSM-1] fix access rights to details page for "EBERO" and "Technical services users" user groups
    - new group "TLD Probe results"
    - new group "gTLD Probe results"
    - new group "ccTLD Probe results"
    - new group "testTLD Probe results"
    - new group "otherTLD Probe results"
    - read permissions on "TLD Probe results" host group for "Technical services users"
    - read permissions on "gTLD Probe results" host group for "EBERO users"
    - read permissions on "Probes" host group for "Technical services users"
    - read permissions on "Probes" host group for "EBERO users"
    - add "gTLD" hosts to "gTLD Probe results" host group
    - add "ccTLD" hosts to "ccTLD Probe results" host group
    - add "testTLD" hosts to "testTLD Probe results" host group
    - add "otherTLD" hosts to "otherTLD Probe results" host group
    - add all TLD hosts to "TLD Probe results" host group
    
    
    git-svn-id: svn://svn.zabbix.com/branches/dev/rsm-dev@67448 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- src/libs/zbxdbupgrade/dbupgrade_3000.c
+++ src/libs/zbxdbupgrade/dbupgrade_3000.c
@@ -29,6 +29,8 @@
 
 #ifndef HAVE_SQLITE3
 
+extern unsigned char	program_type;
+
 static int	DBpatch_3000000(void)
 {
 	return SUCCEED;
@@ -41,6 +43,220 @@ static int	DBpatch_3000100(void)
 	return FAIL;
 }
 
+static int	DBpatch_3000101(void)
+{
+	if (0 != (program_type & ZBX_PROGRAM_TYPE_PROXY))
+		return SUCCEED;
+
+	if (ZBX_DB_OK <= DBexecute("insert into groups (groupid,name,internal,flags) values"
+			" ('190','TLD Probe results','0','0')"))
+	{
+		return SUCCEED;
+	}
+
+	return FAIL;
+}
+
+static int	DBpatch_3000102(void)
+{
+	if (0 != (program_type & ZBX_PROGRAM_TYPE_PROXY))
+		return SUCCEED;
+
+	if (ZBX_DB_OK <= DBexecute("insert into groups (groupid,name,internal,flags) values"
+			" ('200','gTLD Probe results','0','0')"))
+	{
+		return SUCCEED;
+	}
+
+	return FAIL;
+}
+
+static int	DBpatch_3000103(void)
+{
+	if (0 != (program_type & ZBX_PROGRAM_TYPE_PROXY))
+		return SUCCEED;
+
+	if (ZBX_DB_OK <= DBexecute("insert into groups (groupid,name,internal,flags) values"
+			" ('210','ccTLD Probe results','0','0')"))
+	{
+		return SUCCEED;
+	}
+
+	return FAIL;
+}
+
+static int	DBpatch_3000104(void)
+{
+	if (0 != (program_type & ZBX_PROGRAM_TYPE_PROXY))
+		return SUCCEED;
+
+	if (ZBX_DB_OK <= DBexecute("insert into groups (groupid,name,internal,flags) values"
+			" ('220','testTLD Probe results','0','0')"))
+	{
+		return SUCCEED;
+	}
+
+	return FAIL;
+}
+
+static int	DBpatch_3000105(void)
+{
+	if (0 != (program_type & ZBX_PROGRAM_TYPE_PROXY))
+		return SUCCEED;
+
+	if (ZBX_DB_OK <= DBexecute("insert into groups (groupid,name,internal,flags) values"
+			" ('230','otherTLD Probe results','0','0')"))
+	{
+		return SUCCEED;
+	}
+
+	return FAIL;
+}
+
+static int	DBpatch_3000106(void)
+{
+	if (0 != (program_type & ZBX_PROGRAM_TYPE_PROXY))
+		return SUCCEED;
+
+	if (ZBX_DB_OK <= DBexecute("insert into rights (rightid,groupid,permission,id) values ('116','110','2','190')"))
+		return SUCCEED;
+
+	return FAIL;
+}
+
+static int	DBpatch_3000107(void)
+{
+	if (0 != (program_type & ZBX_PROGRAM_TYPE_PROXY))
+		return SUCCEED;
+
+	if (ZBX_DB_OK <= DBexecute("insert into rights (rightid,groupid,permission,id) values ('106','100','2','200')"))
+		return SUCCEED;
+
+	return FAIL;
+}
+
+static int	DBpatch_3000108(void)
+{
+	if (0 != (program_type & ZBX_PROGRAM_TYPE_PROXY))
+		return SUCCEED;
+
+	if (ZBX_DB_OK <= DBexecute("insert into rights (rightid,groupid,permission,id) values ('117','110','2','120')"))
+		return SUCCEED;
+
+	return FAIL;
+}
+
+static int	DBpatch_3000109(void)
+{
+	if (0 != (program_type & ZBX_PROGRAM_TYPE_PROXY))
+		return SUCCEED;
+
+	if (ZBX_DB_OK <= DBexecute("insert into rights (rightid,groupid,permission,id) values ('107','100','2','120')"))
+		return SUCCEED;
+
+	return FAIL;
+}
+
+static int	add_hosts_to_group(const char *tld_type, zbx_uint64_t groupid)
+{
+	DB_RESULT	result;
+	DB_ROW		row;
+	zbx_uint64_t	hostgroupid;
+
+	if (0 != (program_type & ZBX_PROGRAM_TYPE_PROXY))
+		return SUCCEED;
+
+	result = DBselect("select max(hostgroupid) from hosts_groups");
+
+	if (NULL == (row = DBfetch(result)))
+	{
+		DBfree_result(result);
+		return FAIL;
+	}
+
+	ZBX_STR2UINT64(hostgroupid, row[0]);
+
+	result = DBselect(
+			"select h.host"
+			" from hosts h,groups g,hosts_groups hg"
+			" where h.hostid=hg.hostid"
+				" and hg.groupid=g.groupid"
+				" and g.name='%s'", tld_type);
+
+	while (NULL != (row = DBfetch(result)))
+	{
+		DB_RESULT	result2;
+		DB_ROW		row2;
+
+		result2 = DBselect(
+				"select h.hostid"
+				" from hosts h,groups g,hosts_groups hg"
+				" where h.hostid=hg.hostid"
+					" and hg.groupid=g.groupid"
+					" and h.proxy_hostid is not null"
+					" and g.name='TLD %s'", row[0]);
+
+		while (NULL != (row2 = DBfetch(result2)))
+		{
+			zbx_uint64_t	hostid;
+
+			ZBX_STR2UINT64(hostid, row2[0]);
+
+			if (ZBX_DB_OK > DBexecute("insert into hosts_groups (hostgroupid,hostid,groupid) values"
+					" ('" ZBX_FS_UI64 "','" ZBX_FS_UI64 "','" ZBX_FS_UI64 "')",
+					++hostgroupid, hostid, groupid))
+			{
+				DBfree_result(result2);
+				DBfree_result(result);
+
+				return FAIL;
+			}
+
+		}
+
+		DBfree_result(result2);
+	}
+
+	DBfree_result(result);
+
+	if (ZBX_DB_OK > DBexecute(
+			"update ids"
+			" set nextid=" ZBX_FS_UI64
+			" where table_name='hosts_groups'"
+				" and field_name='hostgroupid'",
+			hostgroupid))
+	{
+		return FAIL;
+	}
+
+	return SUCCEED;
+}
+
+static int	DBpatch_3000110(void)
+{
+	return add_hosts_to_group("gTLD", 200);
+}
+
+static int	DBpatch_3000111(void)
+{
+	return add_hosts_to_group("ccTLD", 210);
+}
+
+static int	DBpatch_3000112(void)
+{
+	return add_hosts_to_group("testTLD", 220);
+}
+
+static int	DBpatch_3000113(void)
+{
+	return add_hosts_to_group("otherTLD", 230);
+}
+
+static int	DBpatch_3000114(void)
+{
+	return add_hosts_to_group("TLDs", 190);
+}
+
 #endif
 
 DBPATCH_START(3000)
@@ -49,5 +265,19 @@ DBPATCH_START(3000)
 
 DBPATCH_ADD(3000000, 0, 1)
 DBPATCH_ADD(3000100, 0, 1)	/* Phase 1 */
+DBPATCH_ADD(3000101, 0, 1)	/* new group "TLD Probe results" */
+DBPATCH_ADD(3000102, 0, 1)	/* new group "gTLD Probe results" */
+DBPATCH_ADD(3000103, 0, 1)	/* new group "ccTLD Probe results" */
+DBPATCH_ADD(3000104, 0, 1)	/* new group "testTLD Probe results" */
+DBPATCH_ADD(3000105, 0, 1)	/* new group "otherTLD Probe results" */
+DBPATCH_ADD(3000106, 0, 1)	/* read permissions on "TLD Probe results" host group for "Technical services users" */
+DBPATCH_ADD(3000107, 0, 1)	/* read permissions on "gTLD Probe results" host group for "EBERO users" */
+DBPATCH_ADD(3000108, 0, 1)	/* read permissions on "Probes" host group for "Technical services users" */
+DBPATCH_ADD(3000109, 0, 1)	/* read permissions on "Probes" host group for "EBERO users" */
+DBPATCH_ADD(3000110, 0, 1)	/* add "gTLD" hosts to "gTLD Probe results" host group */
+DBPATCH_ADD(3000111, 0, 1)	/* add "ccTLD" hosts to "ccTLD Probe results" host group */
+DBPATCH_ADD(3000112, 0, 1)	/* add "testTLD" hosts to "testTLD Probe results" host group */
+DBPATCH_ADD(3000113, 0, 1)	/* add "otherTLD" hosts to "otherTLD Probe results" host group */
+DBPATCH_ADD(3000114, 0, 1)	/* add all TLD hosts to "TLD Probe results" host group */
 
 DBPATCH_END()
