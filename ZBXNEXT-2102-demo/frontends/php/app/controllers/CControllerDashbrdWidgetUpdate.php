9f32a524bf8a1eb05dbb562ce3daf3713e67878f
maximt <maximt@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2017-05-26 13:09:53 +0000

    .......... [ZBXNEXT-2102] created branch for demo preview
    git-svn-id: svn://svn.zabbix.com/branches/dev@68596 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBXNEXT-2102-demo/frontends/php/app/controllers/CControllerDashbrdWidgetUpdate.php
@@ -0,0 +1,230 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2001-2017 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+require_once dirname(__FILE__).'/../../include/blocks.inc.php';
+
+class CControllerDashbrdWidgetUpdate extends CController {
+
+	private $widgets;
+
+	public function __construct() {
+		parent::__construct();
+
+		$this->widgets = [];
+	}
+
+	protected function checkInput() {
+		$fields = [
+			'dashboard_id' =>	'db dashboard.dashboardid',
+			'userid' =>         'db dashboard.userid',
+			'name' =>           'not_empty',
+			'widgets' =>		'array',
+			'save' =>			'required|in '.implode(',', [WIDGET_CONFIG_DONT_SAVE, WIDGET_CONFIG_DO_SAVE])
+		];
+
+		$ret = $this->validateInput($fields);
+
+		if ($ret) {
+			/*
+			 * @var array  $widgets
+			 * @var string $widget[]['widgetid']        (optional)
+			 * @var array  $widget[]['pos']             (optional)
+			 * @var int    $widget[]['pos']['row']
+			 * @var int    $widget[]['pos']['col']
+			 * @var int    $widget[]['pos']['height']
+			 * @var int    $widget[]['pos']['width']
+			 * @var string $widget[]['type']
+			 * @var string $widget[]['name']
+			 * @var array  $widget[]['fields']
+			 * @var string $widget[]['fields'][<name>]  (optional)
+			 */
+			foreach ($this->getInput('widgets', []) as $index => $widget) {
+				// TODO VM: check widgetid - if present in $widget, must be existing widget id
+				// TODO VM: (?) instead of adding optional fields, it may be more consistent to add additinal controller
+				//			for checking single widget. That will make this controller more straigt forward.
+				if ($this->getInput('save') == WIDGET_CONFIG_DO_SAVE) {
+					if (!array_key_exists('pos', $widget)) {
+						error(_s('Invalid parameter "%1$s": %2$s.', 'widgets['.$index.']',
+							_s('the parameter "%1$s" is missing', 'pos')
+						));
+						$ret = false;
+					}
+					else {
+						foreach (['row', 'col', 'height', 'width'] as $field) {
+							if (!array_key_exists($field, $widget['pos'])) {
+								error(_s('Invalid parameter "%1$s": %2$s.', 'widgets['.$index.'][pos]',
+									_s('the parameter "%1$s" is missing', $field)
+								));
+								$ret = false;
+							}
+						}
+					}
+				}
+
+				if (!array_key_exists('type', $widget)) {
+					error(_s('Invalid parameter "%1$s": %2$s.', 'widgets['.$index.']',
+						_s('the parameter "%1$s" is missing', 'type')
+					));
+					$ret = false;
+					break;
+				}
+
+				if (!array_key_exists('name', $widget)) {
+					error(_s('Invalid parameter "%1$s": %2$s.', 'widgets['.$index.']',
+						_s('the parameter "%1$s" is missing', 'name')
+					));
+					$ret = false;
+				}
+
+				// AJAX is not sending empty elements,
+				// absence of 'fields' element means, there are no fields for this widget
+				if (!array_key_exists('fields', $widget)) {
+					$widget['fields'] = [];
+				}
+				$widget['form'] = CWidgetConfig::getForm($widget['type'], $widget['fields']);
+				unset($widget['fields']);
+
+				if (($errors = $widget['form']->validate()) !== []) {
+					// TODO VM: Add widget name to each error message.
+					foreach ($errors as $key => $error) {
+						// TODO VM: widgetid will not be present in case of new widget - need to find solution
+						error(_s("Error in widget (id='%s'): %s.", $widget['widgetid'], $error)); // TODO VM: (?) improve error message
+					}
+
+					$ret = false;
+				}
+
+				$this->widgets[] = $widget;
+			}
+		}
+
+		if (!$ret) {
+			$output = [];
+			if (($messages = getMessages()) !== null) {
+				$output['errors'] = $messages->toString();
+			}
+			$this->setResponse(new CControllerResponseData(['main_block' => CJs::encodeJson($output)]));
+		}
+
+		return $ret;
+	}
+
+	protected function checkPermissions() {
+		return ($this->getUserType() >= USER_TYPE_ZABBIX_USER);
+	}
+
+	protected function doAction() {
+		$return = [];
+
+		if ($this->getInput('save') == WIDGET_CONFIG_DO_SAVE) {
+			$dashboard = [
+				'widgets' => []
+			];
+			if ($this->hasInput('dashboard_id')) {
+				$dashboard['dashboardid'] = $this->getInput('dashboard_id');
+			}
+			if ($this->hasInput('name')) {
+				$dashboard['name'] = $this->getInput('name');
+			}
+			if ($this->hasInput('userid')
+				&& in_array($this->getUserType(), [USER_TYPE_ZABBIX_ADMIN, USER_TYPE_SUPER_ADMIN])) {
+				$dashboard['userid'] = $this->getInput('userid');
+			}
+
+			foreach ($this->widgets as $widget) {
+				$upd_widget = [];
+				if (array_key_exists('widgetid', $widget) // widgetid exist during clone action also
+					&& array_key_exists('dashboardid', $dashboard)) {
+					$upd_widget['widgetid'] = $widget['widgetid'];
+				}
+
+				$upd_widget += [
+					'row' => $widget['pos']['row'],
+					'col' => $widget['pos']['col'],
+					'height' => $widget['pos']['height'],
+					'width' => $widget['pos']['width'],
+					'type' => $widget['type'],
+					'name' => $widget['name'],
+					'fields' => $this->prepareFields($widget['form']),
+				];
+
+				$dashboard['widgets'][] = $upd_widget;
+			}
+
+			if (array_key_exists('dashboardid', $dashboard)) {
+				$result = API::Dashboard()->update([$dashboard]);
+				$message = _('Dashboard updated');
+				$error_msg =  _('Failed to update dashboard');
+			} else {
+				$result = API::Dashboard()->create([$dashboard]);
+				$message = _('Dashboard created');
+				$error_msg = _('Failed to create dashboard');
+			}
+
+			if (is_array($result) && array_key_exists('dashboardids', $result)) {
+				$return['redirect'] = (new CUrl('zabbix.php'))
+					->setArgument('action', 'dashboard.view')
+					->setArgument('dashboardid', $result['dashboardids'][0])
+					->getUrl();
+
+				CSession::setValue('messageOk', $message);
+			} else {
+				// TODO AV: improve error messages
+				if (!hasErrorMesssages()) {
+					error(_($error_msg)); // In case of unknown error
+				}
+			}
+		}
+
+		if (($messages = getMessages()) !== null) {
+			// TODO AV: "errors" => "messages"
+			$return['errors'] = $messages->toString();
+		}
+
+		$this->setResponse((new CControllerResponseData(['main_block' => CJs::encodeJson($return)])));
+	}
+
+	/**
+	 * Prepares widget fields for saving.
+	 *
+	 * @param CWidgetForm $form  form object with widget fields
+	 *
+	 * @return array  Array of widget fields ready for saving in API
+	 */
+	protected function prepareFields($form) {
+		// TODO VM: (?) may be good idea to move it to CWidgetForm
+		$fields = [];
+
+		foreach ($form->getFields() as $field) {
+			$save_type = $field->getSaveType();
+
+			$widget_field = [
+				'type' => $save_type,
+				'name' => $field->getName()
+			];
+			$widget_field[CWidgetConfig::getApiFieldKey($save_type)] = $field->getValue();
+
+			$fields[] = $widget_field;
+		}
+
+		return $fields;
+	}
+}
---
+++ ZBXNEXT-2102-demo/frontends/php/app/views/monitoring.dashboard.view.php
@@ -0,0 +1,149 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2001-2017 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+**/
+
+
+$this->addJsFile('dashboard.grid.js');
+$this->addJsFile('multiselect.js');
+$this->includeJSfile('app/views/monitoring.dashboard.view.js.php');
+
+$is_new = !array_key_exists('dashboardid', $data['dashboard']);
+$dashboardid = $is_new ? null : $data['dashboard']['dashboardid'];
+
+$sharing_form = include 'monitoring.dashboard.sharing_form.php';
+list($edit_form, $user_multiselect) = include 'monitoring.dashboard.edit_form.php';
+$breadcrumbs = include 'monitoring.dashboard.breadcrumbs.php';
+
+$dashboard_data = [
+	// name is required for new dashboard creation
+	'name' => $data['dashboard']['name'],
+	'dynamic' => $data['dynamic']
+];
+$dashboard_options = [];
+if (!$is_new) {
+	$dashboard_data['id'] = $dashboardid;
+} else {
+	$dashboard_options['updated'] = true;
+}
+
+$edit_button = new CButton('dashbrd-edit',_('Edit dashboard'));
+if (!$data['dashboard']['editable']) {
+	$edit_button->setAttribute('disabled', 'disabled');
+}
+
+$item_groupid = null;
+$item_hostid = null;
+if ($data['dynamic']['has_dynamic_widgets'] === true) {
+	$item_groupid = ([
+			new CLabel(_('Group'), 'groupid'),
+			(new CDiv())->addClass(ZBX_STYLE_FORM_INPUT_MARGIN),
+			$data['pageFilter']->getGroupsCB()
+		]);
+	$item_hostid = ([
+			new CLabel(_('Host'), 'hostid'),
+			(new CDiv())->addClass(ZBX_STYLE_FORM_INPUT_MARGIN),
+			$data['pageFilter']->getHostsCB()
+		]);
+}
+
+(new CWidget())
+	->setTitle($data['dashboard']['name'])
+	->setControls((new CForm('post', 'zabbix.php?action=dashboard.view'))
+		->cleanItems()
+		->addItem((new CList())
+			// $item_groupid and $item_hostid will be hidden, when 'Edit Dashboard' will be clicked.
+			->addItem($item_groupid)
+			->addItem($item_hostid)
+			// 'Edit dashboard' should be first one in list,
+			// because it will be visually replaced by last item of new list, when clicked
+			->addItem($edit_button)
+			->addItem((new CButton(SPACE))
+				->addClass(ZBX_STYLE_BTN_ACTION)
+				->setTitle(_('Actions'))
+				->setAttribute(
+					'data-menu-popup',
+					CJs::encodeJson([
+						'type' => 'dashboard',
+						'label' => _('Actions'),
+						'items' => [
+							[
+								'name' => 'sharing',
+								'label' => _('Sharing'),
+								'form_data' => [
+									'dashboardid' => $dashboardid,
+								],
+								'disabled' => !$data['dashboard']['editable'] || $is_new
+							],
+							[
+								'name' => 'create',
+								'label' => _('Create new'),
+								'url'  => (new CUrl('zabbix.php'))
+									->setArgument('action', 'dashboard.view')
+									->setArgument('new', '1')
+									->getUrl()
+							],
+							[
+								'name' => 'clone',
+								'label' => _('Clone'),
+								'url'  => (new CUrl('zabbix.php'))
+									->setArgument('action', 'dashboard.view')
+									->setArgument('source_dashboardid', $dashboardid)
+									->getUrl(),
+								'disabled' => $is_new
+							]
+						]
+					])
+				)
+			)
+			->addItem(get_icon('fullscreen', ['fullscreen' => $data['fullscreen']]))
+		)
+	)
+	->addItem((new CList())
+		->addItem($breadcrumbs)
+		->addClass(ZBX_STYLE_OBJECT_GROUP)
+	)
+	->addItem((new CDiv())->addClass(ZBX_STYLE_DASHBRD_GRID_WIDGET_CONTAINER))
+	->addItem($edit_form)
+	->addItem($sharing_form)
+	->show();
+
+/*
+ * Javascript
+ */
+// activating blinking
+$this->addPostJS('jqBlink.blink();');
+
+// Initialize dashboard grid
+$this->addPostJS(
+	'jQuery(".'.ZBX_STYLE_DASHBRD_GRID_WIDGET_CONTAINER.'")'.
+		'.dashboardGrid('. CJs::encodeJson($dashboard_options) . ')'.
+		'.dashboardGrid("setDashboardData", '.CJs::encodeJson($dashboard_data).')'.
+		'.dashboardGrid("setWidgetDefaults", '.CJs::encodeJson($data['widgetDefaults']).')'.
+		'.dashboardGrid("addWidgets", '.CJs::encodeJson($data['grid_widgets']).');'
+);
+
+if ($is_new) {
+	// Edit Form should be opened after multiselect initialization
+	$this->addPostJS(
+		'jQuery(document).on("' . $user_multiselect->getJsEventName() . '", function() {
+				showEditMode();
+				dashbrd_config();
+			});'
+	);
+}
