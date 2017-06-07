0a32f3d66849c4a0471738b8aec141fb3404939e
oleg <oleg@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2017-06-07 12:16:17 +0000

    ..F....... [RSM-1] 218: fixed "Affects rolling week" column in SLA monitoring tests (Oleg)
    git-svn-id: svn://svn.zabbix.com/branches/dev@69011 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- rsm-qa-fix/frontends/php/rsm.tests.php
+++ rsm-qa-fix/frontends/php/rsm.tests.php
@@ -184,7 +184,7 @@ if ($items) {
 
 	$events = API::Event()->get([
 		'output' => API_OUTPUT_EXTEND,
-		'triggerids' => $triggerIds,
+		'objectids' => $triggerIds,
 		'source' => EVENT_SOURCE_TRIGGERS,
 		'object' => EVENT_OBJECT_TRIGGER,
 		'selectTriggers' => API_OUTPUT_EXTEND,
@@ -242,7 +242,7 @@ if ($items) {
 				// get event start time
 				$addEvent = API::Event()->get([
 					'output' => API_OUTPUT_EXTEND,
-					'triggerids' => $event['objectid'],
+					'objectids' => [$event['objectid']],
 					'source' => EVENT_SOURCE_TRIGGERS,
 					'object' => EVENT_OBJECT_TRIGGER,
 					'selectTriggers' => API_OUTPUT_EXTEND,
