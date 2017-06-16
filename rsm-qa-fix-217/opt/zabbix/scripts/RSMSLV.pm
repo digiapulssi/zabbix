2ec0d733da192ec119a9a4b5d2fe5c834dea05c1
gleb <gleb@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2017-06-16 15:52:06 +0000

    ....I..... [RSM-1] ported script changes for support of secure connections to database from ^/branches/dev/rsm-qa-fix-217-scripts
    git-svn-id: svn://svn.zabbix.com/branches/dev@69328 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- rsm-qa-fix-217/opt/zabbix/scripts/RSMSLV.pm
+++ rsm-qa-fix-217/opt/zabbix/scripts/RSMSLV.pm
@@ -789,13 +789,25 @@ sub db_connect
 			unless (defined($section->{$key}));
 	}
 
-	$global_sql = 'DBI:mysql:'.$section->{'db_name'}.':'.$section->{'db_host'};
+	my $db_tls_settings = get_db_tls_settings($section);
+
+	$global_sql = "DBI:mysql:database=$section->{'db_name'};host=$section->{'db_host'};$db_tls_settings";
 
 	$dbh = DBI->connect($global_sql, $section->{'db_user'}, $section->{'db_password'},
 		{
 			PrintError  => 0,
 			HandleError => \&handle_db_error,
 		}) or handle_db_error(DBI->errstr);
+
+	# verify that established database connection uses TLS if there was any hint that it is required in the config
+	unless ($db_tls_settings eq "mysql_ssl=0")
+	{
+		my $rows_ref = db_select("show status like 'Ssl_cipher';");
+
+		fail("established connection is not secure") if ($rows_ref->[0]->[1] eq "");
+
+		dbg("established connection uses \"" . $rows_ref->[0]->[1] . "\" cipher");
+	}
 
 	# improve performance of selects, see
 	# http://search.cpan.org/~capttofu/DBD-mysql-4.028/lib/DBD/mysql.pm
