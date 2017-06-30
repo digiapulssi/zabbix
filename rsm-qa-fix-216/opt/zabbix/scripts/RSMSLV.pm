cf07faa4259b52d2607d71e57e5b7d5294db4049
gleb <gleb@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2017-06-30 19:13:08 +0000

    .......... [RSM-1] updated to the latest trunk
    git-svn-id: svn://svn.zabbix.com/branches/dev@69716 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- rsm-qa-fix-216/opt/zabbix/scripts/RSMSLV.pm
+++ rsm-qa-fix-216/opt/zabbix/scripts/RSMSLV.pm
@@ -791,13 +791,31 @@ sub db_connect
 			unless (defined($section->{$key}));
 	}
 
-	$global_sql = 'DBI:mysql:'.$section->{'db_name'}.':'.$section->{'db_host'};
+	my $db_tls_settings = get_db_tls_settings($section);
+
+	$global_sql = "DBI:mysql:database=$section->{'db_name'};host=$section->{'db_host'};$db_tls_settings";
+
+	dbg($global_sql);
 
 	$dbh = DBI->connect($global_sql, $section->{'db_user'}, $section->{'db_password'},
 		{
 			PrintError  => 0,
 			HandleError => \&handle_db_error,
 		}) or handle_db_error(DBI->errstr);
+
+	# verify that established database connection uses TLS if there was any hint that it is required in the config
+	unless ($db_tls_settings eq "mysql_ssl=0")
+	{
+		my $rows_ref = db_select("show status like 'Ssl_cipher';");
+
+		fail("established connection is not secure") if ($rows_ref->[0]->[1] eq "");
+
+		dbg("established connection uses \"" . $rows_ref->[0]->[1] . "\" cipher");
+	}
+	else
+	{
+		dbg("established connection is unencrypted");
+	}
 
 	# improve performance of selects, see
 	# http://search.cpan.org/~capttofu/DBD-mysql-4.028/lib/DBD/mysql.pm
@@ -864,13 +882,13 @@ sub db_select
 	{
 		my $rows = scalar(@$rows_ref);
 
-		if (scalar(@$rows_ref) == 1)
+		if ($rows == 1)
 		{
 			dbg(join(',', @{$rows_ref->[0]}));
 		}
 		else
 		{
-			dbg("$rows row", ($rows != 1 ? "s" : ""));
+			dbg("$rows rows");
 		}
 	}
 
@@ -3108,12 +3126,10 @@ sub ts_str
 
 	$ts = time() unless ($ts);
 
-	my ($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst) = localtime($ts);
+	# sec, min, hour, mday, mon, year, wday, yday, isdst
+	my ($sec, $min, $hour, $mday, $mon, $year) = localtime($ts);
 
-	$year += 1900;
-	$mon++;
-
-	return sprintf("%4.2d/%2.2d/%2.2d %2.2d:%2.2d:%2.2d", $year, $mon, $mday, $hour, $min, $sec);
+	return sprintf("%.4d%.2d%.2d:%.2d%.2d%.2d", $year + 1900, $mon + 1, $mday, $hour, $min, $sec);
 }
 
 sub ts_full
@@ -3216,14 +3232,12 @@ sub __log
 		$priority = 'UND';
 	}
 
-	$priority .= ':'.$$ if (opt('debug'));
-
 	my $cur_tld = $tld || "";
 	my $server_str = ($server_key ? "\@$server_key " : "");
 
 	if (opt('dry-run') or opt('nolog'))
 	{
-		print {$stdout ? *STDOUT : *STDERR} (ts_str(), " [$priority] ", $server_str, ($cur_tld eq "" ? "" : "$cur_tld: "), __func(), "$msg\n");
+		print {$stdout ? *STDOUT : *STDERR} (sprintf("%6d:", $$), ts_str(), " [$priority] ", $server_str, ($cur_tld eq "" ? "" : "$cur_tld: "), __func(), "$msg\n");
 		return;
 	}
 
