23f0740e17fbbe7aa80bf4f0f75f1c30c0a69c21
dimir <dimir@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2017-07-14 13:52:26 +0000

    ....I..... [RSM-1] 246: fix Probe status calculation in Service availability scripts
    git-svn-id: svn://svn.zabbix.com/branches/dev@70227 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- rsm-qa-fix/opt/zabbix/scripts/slv/rsm.slv.dns.avail.pl
+++ rsm-qa-fix/opt/zabbix/scripts/slv/rsm.slv.dns.avail.pl
@@ -45,19 +45,21 @@ if (opt('tld'))
 }
 else
 {
-        $tlds_ref = get_tlds();
+        $tlds_ref = get_tlds('DNS');	# todo phase 1: change to ENABLED_DNS
 }
 
 while ($period > 0)
 {
 	my ($from, $till, $value_ts) = get_interval_bounds($interval, $clock);
 
+	dbg("selecting period ", selected_period($from, $till), " (value_ts:", ts_str($value_ts), ")");
+
 	$period -= $interval / 60;
 	$clock += $interval;
 
 	next if ($till > $max_avail_time);
 
-	my $probes_ref = get_online_probes($from, $till, $probe_avail_limit, undef);
+	my @probe_names = keys(%{get_probe_times($from, $till, $probe_avail_limit, get_probes('DNS'))});	# todo phase 1: change to ENABLED_DNS
 
 	init_values();
 
@@ -72,7 +74,7 @@ while ($period > 0)
 		}
 
 		process_slv_avail($tld, $cfg_key_in, $cfg_key_out, $from, $till, $value_ts, $cfg_minonline,
-			$probe_avail_limit, $probes_ref, \&check_item_values);
+			$probe_avail_limit, \@probe_names, \&check_item_values);
 	}
 
 	# unset TLD (for the logs)
--- rsm-qa-fix/opt/zabbix/scripts/slv/rsm.slv.rdds.avail.pl
+++ rsm-qa-fix/opt/zabbix/scripts/slv/rsm.slv.rdds.avail.pl
@@ -43,7 +43,7 @@ if (opt('tld'))
 }
 else
 {
-	$tlds_ref = get_tlds('RDDS');
+	$tlds_ref = get_tlds('RDDS');	# todo phase 1: change to ENABLED_RDDS
 }
 
 while ($period > 0)
@@ -57,7 +57,7 @@ while ($period > 0)
 
 	next if ($till > $max_avail_time);
 
-	my $probes_ref = get_online_probes($from, $till, $probe_avail_limit, undef);
+	my @probe_names = keys(%{get_probe_times($from, $till, $probe_avail_limit, get_probes('RDDS'))});	# todo phase 1: change to ENABLED_RDDS
 
 	init_values();
 
@@ -72,7 +72,7 @@ while ($period > 0)
 		}
 
 		process_slv_avail($tld, $cfg_key_in, $cfg_key_out, $from, $till, $value_ts, $cfg_minonline,
-			$probe_avail_limit, $probes_ref, \&check_item_values);
+			$probe_avail_limit, \@probe_names, \&check_item_values);
 	}
 
 	# unset TLD (for the logs)
