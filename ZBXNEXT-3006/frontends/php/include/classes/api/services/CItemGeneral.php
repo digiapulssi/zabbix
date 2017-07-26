digiapulssi/zabbix

commit 8f145ad2a7489121b6875804d84ade26fd5ca4fd

A......... [ZBXNEXT-3006] bugfix for item dependency count validation
git-svn-id: svn://svn.zabbix.com/branches/dev@70481 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

ZBXNEXT-3006/frontends/php/include/classes/api/services/CItemGeneral.php
@@ -1356,22 +1356,28 @@ abstract class CItemGeneral extends CApiService {
 			$items_count = array_key_exists($root_itemid, $items_created)
 				? $items_created[$root_itemid]
 				: 0;
+			$counted_masters = [];
 
 			while ($find_itemids && $dependency_level <= ZBX_DEPENDENT_ITEM_MAX_LEVELS) {
+				// If item was moved to another master item, do not count moved item (and its dependent items)
+				// in old master dependent items count calculation.
+				if (array_key_exists($root_itemid, $items_moved)) {
+					$ignoreids = array_intersect_key($find_itemids, $items_moved[$root_itemid]);
+					$find_itemids = array_diff_key($find_itemids, $ignoreids);
+				}
+				if (array_key_exists($root_itemid, $items_added)
+						&& array_key_exists($dependency_level, $items_added[$root_itemid])) {
+					$find_itemids += $items_added[$root_itemid][$dependency_level];
+				}
 				$find_itemids = $data_provider->get([
 					'output'		=> ['itemid'],
 					'filter' 		=> ['master_itemid' => array_keys($find_itemids)],
 					'preservekeys'	=> true
 				]);
-				if (array_key_exists($root_itemid, $items_added)
-						&& array_key_exists($dependency_level, $items_added[$root_itemid])) {
-					$find_itemids += $items_added[$root_itemid][$dependency_level];
-				}
+
+				$find_itemids = array_diff_key($find_itemids, $counted_masters);
 				$items_count = $items_count + count($find_itemids);
-				// If item was moved to another master item, do not count moved item (and its dependent items)
-				// in old master dependent items count calculation.
-				$ignoreids = array_intersect_key($find_itemids, $items_moved);
-				$find_itemids = array_diff_key($find_itemids, $ignoreids);
+				$counted_masters += $find_itemids;
 				++$dependency_level;
 
 				if ($items_count > ZBX_DEPENDENT_ITEM_MAX_COUNT) {

commit 5a09fcb05abd4257265f61babf81d62806987492

A......... [ZBXNEXT-3006,ZBXNEXT-3883] fixed dependency level scan, bug introduced in r70481
git-svn-id: svn://svn.zabbix.com/branches/dev@70627 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

ZBXNEXT-3006/frontends/php/include/classes/api/services/CItemGeneral.php
@@ -1358,7 +1358,9 @@ abstract class CItemGeneral extends CApiService {
 				: 0;
 			$counted_masters = [];
 
-			while ($find_itemids && $dependency_level <= ZBX_DEPENDENT_ITEM_MAX_LEVELS) {
+			while (($find_itemids || (array_key_exists($root_itemid, $items_added)
+						&& array_key_exists($dependency_level, $items_added[$root_itemid])))
+						&& $dependency_level <= ZBX_DEPENDENT_ITEM_MAX_LEVELS) {
 				// If item was moved to another master item, do not count moved item (and its dependent items)
 				// in old master dependent items count calculation.
 				if (array_key_exists($root_itemid, $items_moved)) {
@@ -1387,7 +1389,9 @@ abstract class CItemGeneral extends CApiService {
 				}
 			};
 
-			if ($find_itemids && $dependency_level > ZBX_DEPENDENT_ITEM_MAX_LEVELS) {
+			if (($find_itemids || (array_key_exists($root_itemid, $items_added)
+					&& array_key_exists($dependency_level, $items_added[$root_itemid])))
+					&& $dependency_level > ZBX_DEPENDENT_ITEM_MAX_LEVELS) {
 				self::exception(ZBX_API_ERROR_PARAMETERS, _s('Incorrect value for field "%1$s": %2$s.',
 					'master_itemid', _('maximum number of dependency levels reached')
 				));