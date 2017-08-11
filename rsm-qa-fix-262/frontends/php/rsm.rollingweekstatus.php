a5a3faade56698ea1a3103eaf20afc0e3b278eff
gleb <gleb@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2017-08-11 09:23:01 +0000

    ..F....... [RSM-1] applied changes in r71111,r71136 from the old rsm-qa-fix-262
    git-svn-id: svn://svn.zabbix.com/branches/dev@71141 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- rsm-qa-fix-262/frontends/php/rsm.rollingweekstatus.php
+++ rsm-qa-fix-262/frontends/php/rsm.rollingweekstatus.php
@@ -375,17 +375,46 @@ foreach ($tlds_by_server as $key => $hosts) {
 
 	if ($hosts) {
 		// get items
-		$items = API::Item()->get(array(
-			'hostids' => array_keys($hosts),
-			'filter' => array(
-				'key_' => array(
-					RSM_SLV_DNS_ROLLWEEK, RSM_SLV_DNSSEC_ROLLWEEK, RSM_SLV_RDDS_ROLLWEEK,
-					RSM_SLV_EPP_ROLLWEEK
-				)
-			),
-			'output' => array('itemid', 'hostid', 'key_', 'lastvalue'),
-			'preservekeys' => true
-		));
+		$item_keys = [RSM_SLV_DNS_ROLLWEEK, RSM_SLV_DNSSEC_ROLLWEEK, RSM_SLV_RDDS_ROLLWEEK, RSM_SLV_EPP_ROLLWEEK];
+
+		$items = [];
+		$db_items = DBselect(
+			'SELECT i.itemid, i.hostid, i.key_'.
+			' FROM items i'.
+			' WHERE '.dbConditionString('i.key_', $item_keys)
+		);
+
+		$i = 0;
+		$history_union = [];
+		while ($item = DBfetch($db_items)) {
+			$items[$item['itemid']] = [
+				'itemid' => $item['itemid'],
+				'hostid' => $item['hostid'],
+				'key_' => $item['key_']
+			];
+
+			$history_union[] = 'SELECT itemid, value'.
+				' FROM history'.
+				' WHERE itemid = '.$item['itemid'].
+					' AND clock = (SELECT MAX(clock) FROM history WHERE itemid = '.$item['itemid'].')';
+
+			if ($i == 3) {
+				$db_histories = DBselect(
+					'SELECT itemid, value'.
+					' FROM ('.implode(' UNION ', $history_union).') result'
+				);
+
+				while ($history = DBfetch($db_histories)) {
+					$items[$history['itemid']]['lastvalue'] = $history['value'];
+				}
+
+				$history_union = [];
+				$i = 0;
+			}
+			else {
+				$i++;
+			}
+		}
 
 		$avail_items = API::Item()->get(array(
 			'hostids' => array_keys($hosts),
