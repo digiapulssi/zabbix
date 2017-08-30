c83226444d16f1ab4d0080bc79263b0137ce7425
wiper <wiper@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2017-08-30 10:15:40 +0000

    .......... [ZBX-12251] reverted last commit
    git-svn-id: svn://svn.zabbix.com/branches/dev@71892 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-12251/src/libs/zbxdbcache/dbcache.c
+++ ZBX-12251/src/libs/zbxdbcache/dbcache.c
@@ -2384,8 +2384,6 @@ int	DCsync_history(int sync_type, int *total_num)
 			DCconfig_clean_items(items, errcodes, history_num);
 			zbx_free(errcodes);
 			zbx_free(items);
-
-			ret = ZBX_DB_OK == txn_error ? SUCCEED : FAIL;
 		}
 		else
 		{
