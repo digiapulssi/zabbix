096967966544938cef51f0d6f32a420f40de28d1
andris <andris@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2017-11-16 10:48:10 +0000

    ...G...... [ZBXNEXT-4216] added logging of 'copy_of' to print_logfile_list()
    git-svn-id: svn://svn.zabbix.com/branches/dev@74673 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-4216/src/zabbix_agent/logfiles.c
+++ ZBXNEXT-4216/src/zabbix_agent/logfiles.c
@@ -445,17 +445,18 @@ static void	print_logfile_list(const struct st_logfile *logfiles, int logfiles_n
 	for (i = 0; i < logfiles_num; i++)
 	{
 		zabbix_log(LOG_LEVEL_DEBUG, "   nr:%d filename:'%s' mtime:%d size:" ZBX_FS_UI64 " processed_size:"
-				ZBX_FS_UI64 " seq:%d incomplete:%d dev:" ZBX_FS_UI64 " ino_hi:" ZBX_FS_UI64 " ino_lo:"
-				ZBX_FS_UI64
+				ZBX_FS_UI64 " seq:%d copy_of:%d incomplete:%d dev:" ZBX_FS_UI64 " ino_hi:" ZBX_FS_UI64
+				" ino_lo:" ZBX_FS_UI64
 				" md5size:%d md5buf:%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
 				i, logfiles[i].filename, logfiles[i].mtime, logfiles[i].size,
-				logfiles[i].processed_size, logfiles[i].seq, logfiles[i].incomplete, logfiles[i].dev,
-				logfiles[i].ino_hi, logfiles[i].ino_lo, logfiles[i].md5size, logfiles[i].md5buf[0],
-				logfiles[i].md5buf[1], logfiles[i].md5buf[2], logfiles[i].md5buf[3],
-				logfiles[i].md5buf[4], logfiles[i].md5buf[5], logfiles[i].md5buf[6],
-				logfiles[i].md5buf[7], logfiles[i].md5buf[8], logfiles[i].md5buf[9],
-				logfiles[i].md5buf[10], logfiles[i].md5buf[11], logfiles[i].md5buf[12],
-				logfiles[i].md5buf[13], logfiles[i].md5buf[14], logfiles[i].md5buf[15]);
+				logfiles[i].processed_size, logfiles[i].seq, logfiles[i].copy_of,
+				logfiles[i].incomplete, logfiles[i].dev, logfiles[i].ino_hi, logfiles[i].ino_lo,
+				logfiles[i].md5size, logfiles[i].md5buf[0], logfiles[i].md5buf[1],
+				logfiles[i].md5buf[2], logfiles[i].md5buf[3], logfiles[i].md5buf[4],
+				logfiles[i].md5buf[5], logfiles[i].md5buf[6], logfiles[i].md5buf[7],
+				logfiles[i].md5buf[8], logfiles[i].md5buf[9], logfiles[i].md5buf[10],
+				logfiles[i].md5buf[11], logfiles[i].md5buf[12], logfiles[i].md5buf[13],
+				logfiles[i].md5buf[14], logfiles[i].md5buf[15]);
 	}
 }
 
