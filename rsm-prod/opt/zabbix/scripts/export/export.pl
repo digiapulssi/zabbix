15b84d02aa9438308879916b7223486e291e834f
gleb <gleb@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2018-03-08 09:05:24 +0000

    ....I..... [RSM-1] rsm1.0.4: 307, 322, 325
    git-svn-id: svn://svn.zabbix.com/branches/dev@78453 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- rsm-prod/opt/zabbix/scripts/export/export.pl
+++ rsm-prod/opt/zabbix/scripts/export/export.pl
@@ -65,9 +65,11 @@ use constant rsm_rdds_probe_result => [
 use constant TARGETS_TMP_DIR => '/opt/zabbix/export-tmp';
 use constant TARGETS_TARGET_DIR => '/opt/zabbix/export';
 
-use constant EXPORT_MAX_CHILDREN_DEFAULT => 16;
+use constant EXPORT_MAX_CHILDREN_DEFAULT => 24;
 use constant EXPORT_MAX_CHILDREN_FLOOR => 1;
 use constant EXPORT_MAX_CHILDREN_CEIL => 128;
+
+use constant EXPORT_LOOP_SLEEP => 2;
 
 parse_opts('probe=s', 'service=s', 'tld=s', 'date=s', 'day=n', 'shift=n', 'force!', 'max-children=n');
 setopt('nolog');
@@ -280,17 +282,21 @@ while ($tld_index < $tld_count)
 	}
 
 	handle_children();
-}
 
-# wait till children finish
-while (children_running() > 0)
-{
-	handle_children();
+	sleep(EXPORT_LOOP_SLEEP);
 }
 
 last if (opt('tld'));
 }	# foreach (@server_keys)
 undef($server_key) unless (opt('tld'));	# keep $server_key if --tld was specified (for __get_false_positives())
+
+# wait till children finish
+while (children_running() > 0)
+{
+	handle_children();
+
+	sleep(EXPORT_LOOP_SLEEP);
+}
 
 # at this point there should be no child processes so we do not care about locking
 
@@ -2348,7 +2354,7 @@ sub __no_cycle_result
 	wrn(uc($service), " service availability result is missing for timestamp ", ts_str($clock), " ($clock).",
 		" This means that either script was not executed or Zabbix server was",
 		" not running at that time. In order to fix this problem please connect",
-		" to appropreate server (check @<server_key> in the beginning of this message)",
+		" to appropriate server (check @<server_key> in the beginning of this message)",
 		" and run the following script:");
 	wrn("  /opt/zabbix/scripts/slv/$avail_key.pl --from $clock");
 }
