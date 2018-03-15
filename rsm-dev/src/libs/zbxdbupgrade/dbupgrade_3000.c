5c6b9ce1eaceaccb3eb4420a475b8283ca09d98f
gleb <gleb@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2018-03-15 06:57:26 +0000

    .......... [RSM-1] updated to the latest rsm-prod, resolved conflicts in DB upgrade
    git-svn-id: svn://svn.zabbix.com/branches/dev@78653 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- rsm-dev/src/libs/zbxdbupgrade/dbupgrade_3000.c
+++ rsm-dev/src/libs/zbxdbupgrade/dbupgrade_3000.c
@@ -1008,6 +1008,17 @@ static int	DBpatch_3000139(void)
 
 static int	DBpatch_3000140(void)
 {
+	if (0 != (program_type & ZBX_PROGRAM_TYPE_PROXY))
+		return SUCCEED;
+
+	if (ZBX_DB_OK > DBexecute("update config set refresh_unsupported=60"))
+		return FAIL;
+
+	return SUCCEED;
+}
+
+static int	DBpatch_3000141(void)
+{
 	/* this is just a direct paste from data.tmpl, with each line quoted and properly indented */
 	static const char	*const data[] = {
 		"ROW   |13000    |130       |-1   |Internal error                                                                                                  |",
@@ -1199,6 +1210,7 @@ DBPATCH_ADD(3000136, 0, 0)	/* add "{$MAX_CPU_LOAD}" and "{$MAX_RUN_PROCESSES}" m
 DBPATCH_ADD(3000137, 0, 0)	/* update "Processor load is too high on {HOST.NAME}" trigger in "Template OS Linux" template and "Zabbix Server" host */
 DBPATCH_ADD(3000138, 0, 0)	/* update "Too many processes running on {HOST.NAME}" trigger in "Template OS Linux" template and "Zabbix Server" host */
 DBPATCH_ADD(3000139, 0, 0)	/* unsuccessful attempt to unlink and link updated "Template OS Linux" template to all hosts it is currently linked to (except "Zabbix Server") */
-DBPATCH_ADD(3000140, 0, 0)	/* update "RSM RDDS rtt" value mapping with new RDDS43 and RDDS80 test error codes */
+DBPATCH_ADD(3000140, 0, 0)	/* lowered "Refresh unsupported items" interval to 60 seconds */
+DBPATCH_ADD(3000141, 0, 0)	/* update "RSM RDDS rtt" value mapping with new RDDS43 and RDDS80 test error codes */
 
 DBPATCH_END()
