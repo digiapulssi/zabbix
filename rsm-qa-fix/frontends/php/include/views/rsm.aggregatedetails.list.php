9b9fec778e1a0335d4e6dacfdf4fb64c038653fb
miks <miks@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2018-04-20 06:51:34 +0000

    ..F....... [RSM-1] :356 fixed empty cell filling by zeros for DNS servers with no results above max RTT
    git-svn-id: svn://svn.zabbix.com/branches/dev@79943 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- rsm-qa-fix/frontends/php/include/views/rsm.aggregatedetails.list.php
+++ rsm-qa-fix/frontends/php/include/views/rsm.aggregatedetails.list.php
@@ -195,14 +195,14 @@ if ($data['type'] == RSM_DNS) {
 		if (array_key_exists('ipv4', $ns_ips)) {
 			foreach (array_keys($ns_ips['ipv4']) as $ipv => $ip) {
 				$error_key = 'udp_'.$ns_name.'_ipv4_'.$ip;
-				$row[] = array_key_exists($error_key, $data['probes_above_max_rtt']) ? $data['probes_above_max_rtt'][$error_key] : '';
+				$row[] = array_key_exists($error_key, $data['probes_above_max_rtt']) ? $data['probes_above_max_rtt'][$error_key] : '0';
 			}
 		}
 
 		if (array_key_exists('ipv6', $ns_ips)) {
 			foreach (array_keys($ns_ips['ipv6']) as $ipv => $ip) {
 				$error_key = 'udp_'.$ns_name.'_ipv6_'.$ip;
-				$row[] = array_key_exists($error_key, $data['probes_above_max_rtt']) ? $data['probes_above_max_rtt'][$error_key] : '';
+				$row[] = array_key_exists($error_key, $data['probes_above_max_rtt']) ? $data['probes_above_max_rtt'][$error_key] : '0';
 			}
 		}
 	}
