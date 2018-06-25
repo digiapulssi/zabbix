digiapulssi/zabbix

commit 55754631346ad615c3a305de72357e4f513d7cac

....I..... [RSM-1] simplify value mapping handling for RDDS
git-svn-id: svn://svn.zabbix.com/branches/dev@81855 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

rsm-qa-fix-182/opt/zabbix/scripts/export/translate.pl
@@ -62,7 +62,7 @@ db_connect();
 my %valuemaps;
 
 $valuemaps{'dns'} = get_valuemaps('dns');
-$valuemaps{'rdds'} = get_valuemaps('rdds');
+$valuemaps{'rdds43'} = $valuemaps{'rdds80'} = get_valuemaps('rdds');
 $valuemaps{'rdap'} = get_valuemaps('rdap');
 
 dw_csv_init();
@@ -166,9 +166,8 @@ sub __translate_tests_line
 	my $test_nsfqdn = dw_get_name(ID_NS_NAME, $columns[10]) || '';
 	my $tld_type = dw_get_name(ID_TLD_TYPE, $columns[11]);
 
-	my $service_str = $test_type;
-	$service_str =~ s/[0-9]+.*//;
-	if ($valuemaps{$service_str}->{$test_rtt})
+	# $test_type is service interface, e. g.: dns, rdds43, rdds80
+	if ($valuemaps{$test_type}->{$test_rtt})
 	{
 		$test_rtt .= " (" . $valuemaps{$service_str}->{$test_rtt} . ")";
 	}

commit ca263067cbf5e4f669659c6be545883ccd206bbe

.......PS. [RSM-1] fixed translate.pl script, broken in r81855
git-svn-id: svn://svn.zabbix.com/branches/dev@82171 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

rsm-qa-fix-182/opt/zabbix/scripts/export/translate.pl
@@ -169,7 +169,7 @@ sub __translate_tests_line
 	# $test_type is service interface, e. g.: dns, rdds43, rdds80
 	if ($valuemaps{$test_type}->{$test_rtt})
 	{
-		$test_rtt .= " (" . $valuemaps{$service_str}->{$test_rtt} . ")";
+		$test_rtt .= " (" . $valuemaps{$test_type}->{$test_rtt} . ")";
 	}
 	elsif ($test_rtt >= 0)
 	{