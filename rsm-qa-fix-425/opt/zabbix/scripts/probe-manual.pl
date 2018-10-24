1657d4a541925d07a2a165e4b3434b4f27d6d9ea
miks <miks@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2018-10-24 10:55:01 +0000

    .......... [RSM-1] :425 updated to latest from ^/branches/dev/rsm-qa-fix (without conflicts)
    git-svn-id: svn://svn.zabbix.com/branches/dev@86004 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- rsm-qa-fix-425/opt/zabbix/scripts/probe-manual.pl
+++ rsm-qa-fix-425/opt/zabbix/scripts/probe-manual.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl
 
 BEGIN
 {
@@ -8,11 +8,12 @@ use lib $MYDIR;
 
 use strict;
 use warnings;
+
 use RSM;
+use RSMSLV;
 use TLD_constants qw(:api);
 use Data::Dumper;
 use Pusher qw(push_to_trapper);
-use RSMSLV;
 
 # todo phase 1: use these 3 from RSMSLV.pm, e. g. create function there that will do what's done in this script
 use constant PROBE_KEY_MANUAL => 'rsm.probe.status[manual]';
