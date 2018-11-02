dbad30f46e4c52f9ba45f4317bd5b6eb14520e0a
miks <miks@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2018-11-02 09:01:31 +0000

    ..F....... [RSM-1] :418 minor code improvement
    git-svn-id: svn://svn.zabbix.com/branches/dev@86357 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- rsm-qa-fix/frontends/php/rsm.particulartests.php
+++ rsm-qa-fix/frontends/php/rsm.particulartests.php
@@ -368,16 +368,14 @@ if ($data['host'] && $data['time'] && $data['slvItemId'] && $data['type'] !== nu
 						$history_value[0]['value'] = (array_sum(zbx_objectValues($history_value, 'value')) > 0) ? 1 : 0;
 						$history_value = $history_value[0];
 
-						if ($history_value) {
-							switch ($history_value['itemid']) {
-								case $_enabled_item_map[RDDS_ENABLED]:
-									$data['tld']['macros'][RSM_TLD_RDDS_ENABLED] = $history_value['value'];
-									break;
+						switch ($history_value['itemid']) {
+							case $_enabled_item_map[RDDS_ENABLED]:
+								$data['tld']['macros'][RSM_TLD_RDDS_ENABLED] = $history_value['value'];
+								break;
 
-								case $_enabled_item_map[RDAP_ENABLED]:
-									$data['tld']['macros'][RSM_RDAP_TLD_ENABLED] = $history_value['value'];
-									break;
-							}
+							case $_enabled_item_map[RDAP_ENABLED]:
+								$data['tld']['macros'][RSM_RDAP_TLD_ENABLED] = $history_value['value'];
+								break;
 						}
 					}
 				}
