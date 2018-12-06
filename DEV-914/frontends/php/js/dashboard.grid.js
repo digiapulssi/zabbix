digiapulssi/zabbix

commit 720792decc264f1cc64eb5ee2182febdf1faf3bb

..F....... [DEV-914] whole placehodler is made clickable for 'add new widget'
git-svn-id: svn://svn.zabbix.com/branches/dev@87580 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

DEV-914/frontends/php/js/dashboard.grid.js
@@ -1215,9 +1215,10 @@
 				inner_box.removeClass('dashbrd-grid-widget-set-size dashbrd-grid-widget-set-position');
 				label.empty().append($('<a>', {
 					href: '#',
-					text: t('Add a new widget'),
-					click: callback
+					text: t('Add a new widget')
 				}));
+
+				placeholder.click(callback);
 			},
 			setPositioning: function() {
 				inner_box.removeClass('dashbrd-grid-widget-set-size').addClass('dashbrd-grid-widget-set-position');

DEV-914/frontends/php/styles/blue-theme.css
@@ -3256,13 +3256,13 @@ td.na-bg ~ td.na-bg, td.normal-bg ~ td.na-bg, td.info-bg ~ td.na-bg, td.average-
     color: #0e4123; }
 
 .info-bg {
-  color: #00268d; }
+  color: #00268e; }
   .info-bg a.link-action,
   .info-bg input[type="radio"]:checked + label {
-    color: #00268d; }
+    color: #00268e; }
 
 .log-info-bg {
-  color: #00268d;
+  color: #00268e;
   background-color: #7499ff; }
 
 .average-bg {

DEV-914/frontends/php/styles/dark-theme.css
@@ -3280,13 +3280,13 @@ td.na-bg ~ td.na-bg, td.normal-bg ~ td.na-bg, td.info-bg ~ td.na-bg, td.average-
     color: #0e4123; }
 
 .info-bg {
-  color: #00268d; }
+  color: #00268e; }
   .info-bg a.link-action,
   .info-bg input[type="radio"]:checked + label {
-    color: #00268d; }
+    color: #00268e; }
 
 .log-info-bg {
-  color: #00268d;
+  color: #00268e;
   background-color: #7499ff; }
 
 .average-bg {
@@ -3647,9 +3647,9 @@ td.na-bg ~ td.na-bg, td.normal-bg ~ td.na-bg, td.info-bg ~ td.na-bg, td.average-
   .subfilter-disable-btn:hover {
     color: #e5e5e5; }
   .subfilter-disable-btn:focus {
-    color: #d8d8d8; }
+    color: #d9d9d9; }
   .subfilter-disable-btn:active {
-    color: #d8d8d8; }
+    color: #d9d9d9; }
 
 .color-picker {
   height: 20px;
@@ -4519,7 +4519,7 @@ svg {
   border-color: #404040; }
 
 .top-subnav a:focus {
-  border-bottom: 4px solid #777777 !important;
+  border-bottom: 4px solid #787878 !important;
   outline: none; }
 
 .list-table.compact-view tr:hover td.na-bg, .list-table.compact-view tr:hover td.normal-bg, .list-table.compact-view tr:hover td.info-bg, .list-table.compact-view tr:hover td.average-bg, .list-table.compact-view tr:hover td.warning-bg, .list-table.compact-view tr:hover td.high-bg, .list-table.compact-view tr:hover td.disaster-bg, .list-table.compact-view tr:hover td.info-bg, .list-table.compact-view tr:hover td.warning-bg, .list-table.compact-view tr:hover td.average-bg, .list-table.compact-view tr:hover td.high-bg, .list-table.compact-view tr:hover td.disaster-bg {

DEV-914/frontends/php/styles/hc-dark.css
@@ -3243,13 +3243,13 @@ td.na-bg ~ td.na-bg, td.normal-bg ~ td.na-bg, td.info-bg ~ td.na-bg, td.average-
     color: #0e4123; }
 
 .info-bg {
-  color: #00268d; }
+  color: #00268e; }
   .info-bg a.link-action,
   .info-bg input[type="radio"]:checked + label {
-    color: #00268d; }
+    color: #00268e; }
 
 .log-info-bg {
-  color: #00268d;
+  color: #00268e;
   background-color: #7499ff; }
 
 .average-bg {
@@ -4953,7 +4953,7 @@ td.na-bg, td.normal-bg, td.info-bg, td.average-bg, td.warning-bg, td.high-bg, td
   color: #f8f8f8; }
 
 .log-info-bg:before {
-  color: #00268d !important;
+  color: #00268e !important;
   background-color: #7499ff !important; }
 
 .info-bg:before, .log-info-bg:before {

DEV-914/frontends/php/styles/hc-light.css
@@ -3243,13 +3243,13 @@ td.na-bg ~ td.na-bg, td.normal-bg ~ td.na-bg, td.info-bg ~ td.na-bg, td.average-
     color: #0e4123; }
 
 .info-bg {
-  color: #00268d; }
+  color: #00268e; }
   .info-bg a.link-action,
   .info-bg input[type="radio"]:checked + label {
-    color: #00268d; }
+    color: #00268e; }
 
 .log-info-bg {
-  color: #00268d;
+  color: #00268e;
   background-color: #7499ff; }
 
 .average-bg {
@@ -3783,7 +3783,7 @@ td.na-bg ~ td.na-bg, td.normal-bg ~ td.na-bg, td.info-bg ~ td.na-bg, td.average-
   background-color: #999; }
 .available::-webkit-scrollbar-thumb, textarea::-webkit-scrollbar-thumb, select::-webkit-scrollbar-thumb, .setup-right-body::-webkit-scrollbar-thumb, .dashbrd-grid-widget-content::-webkit-scrollbar-thumb, .overlay-dialogue.modal .overlay-dialogue-body::-webkit-scrollbar-thumb, .overlay-dialogue .maps-container::-webkit-scrollbar-thumb, .notif-body::-webkit-scrollbar-thumb, .debug-output::-webkit-scrollbar-thumb, .overlay-descr::-webkit-scrollbar-thumb, .msg-details ul::-webkit-scrollbar-thumb, .overflow-table::-webkit-scrollbar-thumb {
   background-color: #f9f9f9;
-  border: 1px solid #acacac; }
+  border: 1px solid #adadad; }
 
 .overflow-table::-webkit-scrollbar {
   height: 9px; }
@@ -4439,7 +4439,7 @@ svg {
     background: #555; }
   .range-control.range-control-focus .range-control-thumb {
     background: #555;
-    box-shadow: 0 0 0 2px #6e6e6e; }
+    box-shadow: 0 0 0 2px #6f6f6f; }
   .range-control.range-control-focus .range-control-progress {
     background: #555; }
   .range-control.disabled {
@@ -4628,7 +4628,7 @@ button:focus {
   -webkit-box-shadow: 0 0 0 2px #111111;
   -moz-box-shadow: 0 0 0 2px #111111;
   box-shadow: 0 0 0 2px #111111;
-  background-color: #6e6e6e; }
+  background-color: #6f6f6f; }
 
 .btn-alt:focus {
   -webkit-box-shadow: 0 0 0 2px #111111;
@@ -4915,7 +4915,7 @@ td.na-bg, td.normal-bg, td.info-bg, td.average-bg, td.warning-bg, td.high-bg, td
   color: #555; }
 
 .log-info-bg:before {
-  color: #00268d !important;
+  color: #00268e !important;
   background-color: #7499ff !important; }
 
 .info-bg:before, .log-info-bg:before {
@@ -5144,19 +5144,19 @@ td.inactive-bg {
 
 .ui-tabs-nav .btn-time:hover,
 .ui-tabs-nav .filter-trigger:hover {
-  background-color: #3b3b3b; }
+  background-color: #3c3c3c; }
 .ui-tabs-nav .btn-time:focus,
 .ui-tabs-nav .filter-trigger:focus {
   -webkit-box-shadow: 0 0 0 2px #111111;
   -moz-box-shadow: 0 0 0 2px #111111;
   box-shadow: 0 0 0 2px #111111;
-  background-color: #6e6e6e; }
+  background-color: #6f6f6f; }
 .ui-tabs-nav .ui-state-focus .btn-time,
 .ui-tabs-nav .ui-state-focus .filter-trigger {
   -webkit-box-shadow: 0 0 0 2px #111111;
   -moz-box-shadow: 0 0 0 2px #111111;
   box-shadow: 0 0 0 2px #111111;
-  background-color: #6e6e6e; }
+  background-color: #6f6f6f; }
 .ui-tabs-nav .ui-state-focus.ui-tabs-active .btn-time,
 .ui-tabs-nav .ui-state-focus.ui-tabs-active .filter-trigger {
   -webkit-box-shadow: 0 0 0 2px #111111;
@@ -5183,7 +5183,7 @@ td.inactive-bg {
   border-color: #484848; }
 .btn-time-right:focus,
 .btn-time-left:focus {
-  background-color: #6e6e6e; }
+  background-color: #6f6f6f; }
 
 .time-quick li a:focus {
   -webkit-box-shadow: 0 0 0 2px #111111;

commit 305b3fdcd35889176fc559e5b4c4866f1ef87285

..F....... [DEV-914] fixed add new widget with custom size caused by r87580
git-svn-id: svn://svn.zabbix.com/branches/dev@87586 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

DEV-914/frontends/php/js/dashboard.grid.js
@@ -1221,10 +1221,12 @@
 				placeholder.click(callback);
 			},
 			setPositioning: function() {
+				placeholder.off('click');
 				inner_box.removeClass('dashbrd-grid-widget-set-size').addClass('dashbrd-grid-widget-set-position');
 				updateLabelVisibility();
 			},
 			setResizing: function() {
+				placeholder.off('click');
 				inner_box.removeClass('dashbrd-grid-widget-set-position').addClass('dashbrd-grid-widget-set-size');
 				updateLabelVisibility();
 			},
@@ -1251,7 +1253,6 @@
 			}
 
 			var dimension = $.extend({}, data.add_widget_dimension);
-
 			if (dimension.width == 1 && dimension.height == 2) {
 				delete dimension.width;
 				delete dimension.height;
@@ -1279,7 +1280,7 @@
 				return;
 			}
 
-			data.add_widget_dimension = {};
+			//data.add_widget_dimension = {};
 
 			if (data.widgets.length) {
 				data.new_widget_placeholder.container.hide();