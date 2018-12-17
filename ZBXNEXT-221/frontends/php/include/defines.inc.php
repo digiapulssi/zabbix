3023ad25d6efa55062dd0c5962a3c1d4565fc5a6
talbergs <talbergs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2018-12-17 10:27:46 +0000

    ..F....... [ZBXNEXT-221] reverted css changes
    git-svn-id: svn://svn.zabbix.com/branches/dev@87868 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-221/frontends/php/include/defines.inc.php
+++ ZBXNEXT-221/frontends/php/include/defines.inc.php
@@ -1557,7 +1557,6 @@ define('ZBX_STYLE_LINK_ACTION', 'link-action');
 define('ZBX_STYLE_LINK_ALT', 'link-alt');
 define('ZBX_STYLE_LIST_HOR_CHECK_RADIO', 'list-hor-check-radio');
 define('ZBX_STYLE_LIST_CHECK_RADIO', 'list-check-radio');
-define('ZBX_STYLE_GRID_CHECK_RADIO', 'grid-check-radio');
 define('ZBX_STYLE_LIST_TABLE', 'list-table');
 define('ZBX_STYLE_LIST_TABLE_FOOTER', 'list-table-footer');
 define('ZBX_STYLE_LIST_VERTICAL_ACCORDION', 'list-vertical-accordion');
