d6b415cb94ec61af34ebd33d2cab44efcd7cd67c
dimir <dimir@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2019-01-16 13:21:32 +0000

    ....I..... [RSM-1] add sla-api-recent.pl script and modify existing SLA-API implementation to support new script
    git-svn-id: svn://svn.zabbix.com/branches/dev@88719 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- rsm-dev/src/libs/zbxdbupgrade/dbupgrade_3000.c
+++ rsm-dev/src/libs/zbxdbupgrade/dbupgrade_3000.c
@@ -3047,7 +3047,7 @@ static int	DBpatch_3000234(void)
 		return FAIL;
 	}
 
-	/* delete obsoleted records */
+	/* add constraint */
 	if (ZBX_DB_OK > DBexecute(
 			"alter table `lastvalue`"
 			" add constraint `c_lastvalue_1`"
@@ -3180,6 +3180,41 @@ static int	DBpatch_3000236(void)
 {
 	return DBpatch_3000226();
 }
+
+static int	DBpatch_3000300(void)
+{
+	return SUCCEED;
+}
+
+static int	DBpatch_3000301(void)
+{
+	const ZBX_TABLE table =
+			{"lastvalue_str", "itemid", 0,
+				{
+					{"itemid", NULL, NULL, NULL, 0, ZBX_TYPE_ID, ZBX_NOTNULL, 0},
+					{"clock", "0", NULL, NULL, 0, ZBX_TYPE_INT, ZBX_NOTNULL, 0},
+					{"value", "", NULL, NULL, 255, ZBX_TYPE_CHAR, ZBX_NOTNULL, 0},
+					{0}
+				},
+				NULL
+			};
+
+	if (SUCCEED != DBcreate_table(&table))
+		return FAIL;
+
+	/* add constraint */
+	if (ZBX_DB_OK > DBexecute(
+			"alter table `lastvalue_str`"
+			" add constraint `c_lastvalue_str_1`"
+				" foreign key (`itemid`) references `items` (`itemid`)"
+			" on delete cascade"))
+	{
+		return FAIL;
+	}
+
+	return SUCCEED;
+}
+
 #endif
 
 DBPATCH_START(3000)
@@ -3263,5 +3298,7 @@ DBPATCH_ADD(3000233, 0, 0)	/* change global macro value {$PROBE.INTERNAL.ERROR.I
 DBPATCH_ADD(3000234, 0, 0)	/* add constraint on lastvalue table to delete obsoleted itemids */
 DBPATCH_ADD(3000235, 0, 0)	/* add trigger for item rsm.probe.status[manual] to alert on Probe knock out */
 DBPATCH_ADD(3000236, 0, 0)	/* disable "RDAP availability" items on hosts where RDAP is disabled (again) */
+DBPATCH_ADD(3000300, 0, 0)	/* Phase 3 */
+DBPATCH_ADD(3000301, 0, 0)	/* add lastvalue_str table */
 
 DBPATCH_END()
