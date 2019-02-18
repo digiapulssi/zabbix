9b7c49d04fcb9f6ffa89b972d7869ccea9e7af20
talbergs <talbergs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2019-02-18 15:34:43 +0000

    ..F....... [ZBX-15464] fixed comparison
    git-svn-id: svn://svn.zabbix.com/branches/dev@89981 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-15464/frontends/php/include/classes/setup/CFrontendSetup.php
+++ ZBX-15464/frontends/php/include/classes/setup/CFrontendSetup.php
@@ -118,7 +118,7 @@ class CFrontendSetup {
 	 */
 	public function checkPhpMemoryLimit() {
 		$current = ini_get('memory_limit');
-		$check = ($current == '-1' || bccomp(str2mem($current), self::MIN_PHP_MEMORY_LIMIT) > 0);
+		$check = ($current == '-1' || bccomp(str2mem($current), self::MIN_PHP_MEMORY_LIMIT) >= 0);
 
 		return [
 			'name' => _s('PHP option "%1$s"', 'memory_limit'),
@@ -141,7 +141,7 @@ class CFrontendSetup {
 			'name' => _s('PHP option "%1$s"', 'post_max_size'),
 			'current' => $current,
 			'required' => mem2str(self::MIN_PHP_POST_MAX_SIZE),
-			'result' => (bccomp(str2mem($current), self::MIN_PHP_POST_MAX_SIZE) > 0) ? self::CHECK_OK : self::CHECK_FATAL,
+			'result' => (bccomp(str2mem($current), self::MIN_PHP_POST_MAX_SIZE) >= 0) ? self::CHECK_OK : self::CHECK_FATAL,
 			'error' => _s('Minimum required size of PHP post is %s (configuration option "post_max_size").', mem2str(self::MIN_PHP_POST_MAX_SIZE))
 		];
 	}
@@ -158,7 +158,7 @@ class CFrontendSetup {
 			'name' => _s('PHP option "%1$s"', 'upload_max_filesize'),
 			'current' => $current,
 			'required' => mem2str(self::MIN_PHP_UPLOAD_MAX_FILESIZE),
-			'result' => (bccomp(str2mem($current), self::MIN_PHP_UPLOAD_MAX_FILESIZE) > 0) ? self::CHECK_OK : self::CHECK_FATAL,
+			'result' => (bccomp(str2mem($current), self::MIN_PHP_UPLOAD_MAX_FILESIZE) >= 0) ? self::CHECK_OK : self::CHECK_FATAL,
 			'error' => _s('Minimum required PHP upload filesize is %s (configuration option "upload_max_filesize").', mem2str(self::MIN_PHP_UPLOAD_MAX_FILESIZE))
 		];
 	}
