d60da5d0eff55a65a1fdcce31efdac6c7cd3189b
dimir <dimir@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2019-03-01 09:35:05 +0000

    ..F.I..PS. [RSM-1] rsm1.1.1: mark DNS errors -252, -652 in mappings as obsoleted instead of removing
    git-svn-id: svn://svn.zabbix.com/branches/dev@90419 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- rsm-prod/src/libs/zbxdbupgrade/dbupgrade_3000.c
+++ rsm-prod/src/libs/zbxdbupgrade/dbupgrade_3000.c
@@ -3183,7 +3183,10 @@ static int	DBpatch_3000236(void)
 
 static int      DBpatch_3000237(void)
 {
-	if (ZBX_DB_OK > DBexecute("delete from mappings where mappingid in (12019,12073)"))
+	if (ZBX_DB_OK > DBexecute("update mappings set newvalue='DNS UDP - Querying for a non existent domain - Expecting NXDOMAIN RCODE but got NOERROR (obsolete)' where mappingid=12019"))
+		return FAIL;
+
+	if (ZBX_DB_OK > DBexecute("update mappings set newvalue='DNS TCP - Querying for a non existent domain - Expecting NXDOMAIN RCODE but got NOERROR (obsolete)' where mappingid=12073"))
 		return FAIL;
 
 	return SUCCEED;
@@ -3279,7 +3282,7 @@ DBPATCH_ADD(3000233, 0, 0)	/* change global macro value {$PROBE.INTERNAL.ERROR.I
 DBPATCH_ADD(3000234, 0, 0)	/* add constraint on lastvalue table to delete obsoleted itemids */
 DBPATCH_ADD(3000235, 0, 0)	/* add trigger for item rsm.probe.status[manual] to alert on Probe knock out */
 DBPATCH_ADD(3000236, 0, 0)	/* disable "RDAP availability" items on hosts where RDAP is disabled (again) */
-DBPATCH_ADD(3000237, 0, 0)	/* remove obsoleted DNS errors -252, -652 from mappings */
+DBPATCH_ADD(3000237, 0, 0)	/* mark DNS errors -252, -652 in mappings as obsoleted */
 DBPATCH_ADD(3000238, 0, 0)	/* increase "value" field of "lastvalue" table by double(24,4) to accept bigint values */
 
 DBPATCH_END()
