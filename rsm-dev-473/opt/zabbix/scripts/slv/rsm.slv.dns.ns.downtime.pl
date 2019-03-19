4a89cb968cc439387cccbe42b47eeb67083e101c
yurii <yurii@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2019-03-19 14:30:09 +0000

    ....I..... [RSM-1] 473: using SLV_UNAVAILABILITY_LIMIT
    git-svn-id: svn://svn.zabbix.com/branches/dev@91237 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- rsm-dev-473/opt/zabbix/scripts/slv/rsm.slv.dns.ns.downtime.pl
+++ rsm-dev-473/opt/zabbix/scripts/slv/rsm.slv.dns.ns.downtime.pl
@@ -20,17 +20,13 @@ fail_if_running();
 set_slv_config(get_rsm_config());
 db_connect();
 
-# # we don't know the rollweek bounds yet so we assume it ends at least few minutes back
-# my $delay = get_dns_udp_delay(getopt('now') // time() - ROLLWEEK_SHIFT_BACK);
-# 
-# my ($month_start, $till, $cycle_start) = get_downtime_bounds($delay, getopt('now'));
-
 my $month_first_cycle = current_month_first_cycle();
 my $month_latest_cycle = current_month_latest_cycle();
 
 init_values();
 process_values();
 send_values();
+
 slv_exit(SUCCESS);
 
 sub process_values
@@ -83,6 +79,7 @@ sub process_slv_item
 	my $slv_itemid = shift;
 	my $slv_itemkey = shift;
 
+	# extract nsip pair and process relevant rtt items
 	if ($slv_itemkey =~ /\[(.+,.+)\]$/)
 	{
 		process_rtt_items($tld, $slv_itemid, $slv_itemkey, $1);
@@ -91,14 +88,6 @@ sub process_slv_item
 	{
 		fail("missing ns,ip pair in item key '$slv_itemkey'");
 	}
-}
-
-sub get_dns_udp_rtt_items_by_nsip_pairs
-{
-	my $nsip = shift;
-
-	return db_select("select itemid,key_ from items".
-		" where key_ like '$rtt_item_key_pattern\[\%$nsip]' and templateid is not null");
 }
 
 sub rtt_itemids
@@ -123,8 +112,10 @@ sub process_rtt_items # for a particular slv item
 
 	my ($slv_lastvalue, $slv_clock) = @{get_slv_lastvalue_and_clock_by_itemid($slv_itemid)};
 
-	if ($slv_clock >= $month_latest_cycle)
+	print "$slv_clock : $month_latest_cycle\n";
+	if ($slv_clock > $month_latest_cycle)
 	{
+		print "No cycles!\n";
 		return;
 	}
 
@@ -136,6 +127,7 @@ sub process_rtt_items # for a particular slv item
 
 	if (!defined($rtt_item_history) or 0 == scalar(@{$rtt_item_history}))
 	{
+		# assuming up if no rtt data
 		$cycle_down = 0;
 	}
 	else
@@ -152,11 +144,14 @@ sub process_rtt_items # for a particular slv item
 			}
 		}
 
-		$cycle_down = ($bad_probe_count >= 0.5 * $rtt_item_count) ? 1 : 0;
+		# down if more than half of probes returned down
+		my $unavail_limit = (SLV_UNAVAILABILITY_LIMIT * 0.01) * $rtt_item_count;
+		$cycle_down = ($bad_probe_count > $unavail_limit) ? 1 : 0;
 	}
 
 	my $new_value = $slv_lastvalue + $cycle_down;
 
+	print "$cycle_down, $new_value, $slv_clock\n";
 	push_value($tld, $slv_itemkey, $slv_clock + 60, $new_value);
 }
 
@@ -166,7 +161,7 @@ sub get_slv_lastvalue_and_clock_by_itemid
 
 	my $rows_ref = db_select("select value,clock from lastvalue where itemid=$itemid");
 
-	# start with the first cycle of the current month if there are no previous slv values
+	# start with the latest cycle of the current month if there are no previous slv values
 	return $rows_ref->[0] // [0, $month_first_cycle];
 }
 
@@ -180,6 +175,14 @@ sub get_rtt_item_history
 		" and clock between $from and $till");
 }
 
+sub get_dns_udp_rtt_items_by_nsip_pairs
+{
+	my $nsip = shift;
+
+	return db_select("select itemid,key_ from items".
+		" where key_ like '$rtt_item_key_pattern\[\%$nsip]' and templateid is not null");
+}
+
 sub current_month_first_cycle
 {
 	my $dt = DateTime->now();
@@ -189,5 +192,6 @@ sub current_month_first_cycle
 
 sub current_month_latest_cycle
 {
+	# we don't know the rollweek bounds yet so we assume it ends at least few minutes back
 	return cycle_start(time(), 60) - ROLLWEEK_SHIFT_BACK;
 }
