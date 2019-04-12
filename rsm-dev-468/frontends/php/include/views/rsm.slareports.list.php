94aa08e7b0c33d41ea2ab3b24ef0cde92665d777
gcalenko <gcalenko@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2019-04-12 13:47:53 +0000

    .......... [RSM-1] 468: created new development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@92627 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ rsm-dev-468/frontends/php/include/views/rsm.slareports.list.php
@@ -0,0 +1,180 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2001-2016 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+$widget = (new CWidget())->setTitle(_('SLA report'));
+
+$months = range(1, 12);
+$years = range(SLA_MONITORING_START_YEAR, date('Y', time()));
+
+$widget->addItem(
+	(new CFilter('web.rsm.slareports.filter.state'))->addColumn(
+		(new CFormList())
+			->addVar('filter_set', 1)
+			->addRow(_('TLD'), (new CTextBox('filter_search', $data['filter_search']))
+				->setWidth(ZBX_TEXTAREA_FILTER_STANDARD_WIDTH)
+				->setAttribute('autocomplete', 'off')
+			)
+			->addRow(_('Period'), [
+				new CComboBox('filter_month', $data['filter_month'], null, array_combine($months,
+					array_map('getMonthCaption', $months))),
+				SPACE,
+				new CComboBox('filter_year', $data['filter_year'], null, array_combine($years, $years))
+			])
+	)
+);
+
+$table = (new CTableInfo())->setHeader([
+	_('Service'),
+	_('FQDN and IP'),
+	_('From'),
+	_('To'),
+	_('SLV'),
+	_('Monthly SLR')
+]);
+
+if (!$data['tld']) {
+	return $widget->addItem($table);
+}
+
+// TLD details.
+$widget->additem((new CDiv())
+	->addClass(ZBX_STYLE_TABLE_FORMS_CONTAINER)
+	->addItem([
+		bold(_s('Period: %1$s - %2$s', gmdate('Y/m/d H:i:s', $data['start_time']),
+			gmdate('Y/m/d H:i:s', $data['end_time']))), BR(),
+		bold(_s('Generation time: %1$s', gmdate('dS F Y, H:i:s e', time()))), BR(),
+		bold(_s('TLD: %1$s', $data['tld']['name'])), BR(),
+		bold(_('Server: ')), new CLink($data['server'], $data['rolling_week_url'])
+	])
+);
+
+// DNS Server Availability.
+$table->addRow([
+		bold(_('DNS Server Availability')),
+		'-',
+		'-',
+		'-',
+		_s('%d (minutes of downtime)', $data['values'][RSM_SLV_DNS_DOWNTIME]['slv']),
+		_s('%d (minutes of downtime)', $data['macro'][RSM_SLV_NS_DOWNTIME])
+	],
+	($data['values'][RSM_SLV_DNS_DOWNTIME]['slv'] > $data['macro'][RSM_SLV_NS_DOWNTIME]) ? 'red-bg' : null
+);
+
+// DNS Name Server availability.
+foreach ($data['values'] as $item) {
+	if (!array_key_exists('nsitem', $item)) {
+		continue;
+	}
+
+	$table->addRow([
+			_('DNS Name Server availability'),
+			implode(', ', array_filter([$item['host'], $item['ip']], 'strlen')),
+			gmdate('Y-m-d H:i:s e', $item['from']),
+			gmdate('Y-m-d H:i:s e', $item['to']),
+			_s('%1$s (minutes of downtime)', $item['slv']),
+			_s('%1$s (minutes of downtime)', $data['macro'][RSM_SLV_NS_DOWNTIME])
+		],
+		($item['slv'] > $data['macro'][RSM_SLV_NS_DOWNTIME]) ? 'red-bg' : null
+	);
+}
+
+// TCP/UDP DNS Resolution.
+$table
+	->addRow([
+			_('DNS TCP Resolution RTT'),
+			'-',
+			'-',
+			'-',
+			_s('%1$s %% (queries <= %2$s ms)', $data['values'][RSM_SLV_DNS_TCP_RTT_PFAILED]['slv'],
+				$data['macro'][RSM_DNS_TCP_RTT_LOW]
+			),
+			_s('<= %1$s ms, for at least %2$s %% of queries', $data['macro'][RSM_DNS_TCP_RTT_LOW],
+				$data['macro'][RSM_SLV_DNS_TCP_RTT]
+			)
+		],
+		($data['values'][RSM_SLV_DNS_TCP_RTT_PFAILED]['slv'] > $data['macro'][RSM_SLV_DNS_TCP_RTT])
+			? 'red-bg' : null
+	)->addRow([
+			_('DNS UDP Resolutioin RTT'),
+			'-',
+			'-',
+			'-',
+			_s('%1$s %% (queries <= %2$s ms)', $data['values'][RSM_SLV_DNS_UDP_RTT_PFAILED]['slv'],
+				$data['macro'][RSM_DNS_TCP_RTT_LOW]
+			),
+			_s('<= %1$s ms, for at least %2$s %% of queries', $data['macro'][RSM_DNS_TCP_RTT_LOW],
+				$data['macro'][RSM_SLV_DNS_TCP_RTT]
+			)
+		],
+		($data['values'][RSM_SLV_DNS_UDP_RTT_PFAILED]['slv'] > $data['macro'][RSM_SLV_DNS_TCP_RTT])
+			? 'red-bg' : null
+);
+
+// RDDS Availability.
+if (array_key_exists(RSM_SLV_RDDS_DOWNTIME, $data['values'])) {
+	$table->addRow([
+			bold(_('RDDS Availability')),
+			'-',
+			'-',
+			'-',
+			_s('%1$s (minutes of downtime)', $data['values'][RSM_SLV_RDDS_DOWNTIME]['slv']),
+			_s('<= %1$s min of downtime', $data['macro'][RSM_DNS_UDP_RTT_LOW])
+		],
+		($data['values'][RSM_SLV_RDDS_DOWNTIME]['slv'] > $data['macro'][RSM_DNS_UDP_RTT_LOW])
+			? 'red-bg' : null
+	)->addRow([
+			_('RDDS Query RTT'),
+			'-',
+			'-',
+			'-',
+			_s('%1$s %% (queries <= %2$s ms)', $data['values'][RSM_SLV_RDDS_RTT_PFAILED]['slv'],
+				$data['macro'][RSM_RDDS_RTT_LOW]
+			),
+			_s('<= %1$s ms, for at least %2$s %% of the queries', $data['macro'][RSM_RDDS_RTT_LOW],
+				$data['macro'][RSM_SLV_MACRO_RDDS_RTT]
+			)
+		],
+		($data['values'][RSM_SLV_RDDS_RTT_PFAILED]['slv'] > $data['macro'][RSM_SLV_MACRO_RDDS_RTT])
+			? 'red-bg' : null
+	);
+}
+
+// Append download button.
+$download_button = $data['tld']
+	? (new CButton('download-report', 'Download XML'))->onClick(sprintf(
+		'javascript: document.location = "rsm.slareports-download.php?%s"',
+		http_build_query([
+			'action' => 'download-xml',
+			'server' => $data['server_nr'],
+			'tld' => $data['tld']['host'],
+			'year' => $data['filter_year'],
+			'month' => $data['filter_month'],
+			'source_url' => $data['source_url']
+		])
+	))
+	: (new CButton('download-report', 'Download XML'))->setEnabled(false);
+
+return $widget->addItem([
+	$table,
+	(new CDiv())
+		->addItem($download_button)
+		->addClass('action-buttons')
+]);
---
+++ rsm-dev-468/opt/zabbix/scripts/slv/rsm.slv.dns.ns.downtime.pl
@@ -0,0 +1,196 @@
+#!/usr/bin/perl
+#
+# Minutes of DNS downtime during running month for particular nameservers
+
+BEGIN { our $MYDIR = $0; $MYDIR =~ s,(.*)/.*/.*,$1,; $MYDIR = '..' if ($MYDIR eq $0); }
+use lib $MYDIR;
+use strict;
+use warnings;
+use RSM;
+use RSMSLV;
+use TLD_constants qw(:groups :api);
+use Data::Dumper;
+use DateTime;
+
+parse_slv_opts();
+fail_if_running();
+set_slv_config(get_rsm_config());
+db_connect();
+
+use constant AVAIL_KEY_PATTERN => 'rsm.slv.dns.ns.avail';
+use constant DOWNTIME_KEY_PATTERN => 'rsm.slv.dns.ns.downtime';
+
+my $max_cycles = (opt('cycles') ? getopt('cycles') : slv_max_cycles('dns'));
+
+init_values();
+process_values();
+send_values();
+
+slv_exit(SUCCESS);
+
+sub process_values
+{
+	foreach my $tld (@{get_tlds_and_hostids(opt('tld') ? getopt('tld') : undef)})
+	{
+		process_tld(@{$tld});
+	}
+}
+
+sub process_tld
+{
+	my $tld = shift;
+	my $hostid = shift;
+
+	process_ns_items($tld, $hostid, get_ns_items($tld, $hostid));
+}
+
+sub get_ns_items
+{
+	my $tld = shift;
+	my $hostid = shift;
+
+	my $rows_avail = db_select(
+		"select itemid,key_".
+		" from items".
+		" where hostid=$hostid".
+			" and key_ like '" . AVAIL_KEY_PATTERN . "[%'"
+	);
+
+	fail("failed to obtain ns avail items") unless (scalar(@{$rows_avail}));
+
+	my $rows_downtime = db_select(
+		"select itemid,key_".
+		" from items".
+		" where hostid=$hostid".
+			" and key_ like '" . DOWNTIME_KEY_PATTERN . "[%'"
+	);
+
+	fail("failed to obtain ns downtime items") unless (scalar(@{$rows_downtime}));
+
+	if (scalar(@{$rows_avail}) != scalar(@{$rows_downtime}))
+	{
+		fail("got different number of ns avail and downtime items");
+	}
+
+	my $items_by_nsip = {};
+
+	foreach my $row (@{$rows_avail})
+	{
+		my $itemid = $row->[0];
+		my $itemkey = $row->[1];
+
+		if ($itemkey =~ /\[(.+,.+)\]$/)
+		{
+			$items_by_nsip->{$1} = {'avail_itemid' => $itemid};
+		}
+		else
+		{
+			fail("cannot extract ns,ip pair from ns avail item key '$itemkey'");
+		}
+	}
+
+	foreach my $row (@{$rows_downtime})
+	{
+		my $itemid = $row->[0];
+		my $itemkey = $row->[1];
+
+		if ($itemkey =~ /\[(.+,.+)\]$/)
+		{
+			if (defined($items_by_nsip->{$1}))
+			{
+				$items_by_nsip->{$1}{'downtime_itemid'} = $itemid;
+				$items_by_nsip->{$1}{'downtime_key'} = $itemkey;
+			}
+			else
+			{
+				fail("no ns avail item for ns,ip pair '$1'");
+			}
+		}
+		else
+		{
+			fail("cannot extract ns,ip pair from ns downtime item key '$itemkey'");
+		}
+	}
+
+	return $items_by_nsip;
+}
+
+sub process_ns_items
+{
+	my $tld = shift;
+	my $hostid = shift;
+	my $items_by_nsip = shift;
+
+	for my $nsip (keys(%{$items_by_nsip}))
+	{
+		my $items = $items_by_nsip->{$nsip};
+
+		calculate_downtime_values($tld, $nsip,
+				$items->{'avail_itemid'}, $items->{'downtime_itemid'}, $items->{'downtime_key'});
+	}
+}
+
+sub calculate_downtime_values
+{
+	my $tld = shift;
+	my $nsip = shift;
+	my $avail_itemid = shift;
+	my $downtime_itemid = shift;
+	my $downtime_key = shift;
+
+	my $avail_clock;
+	my $avail_value;
+
+	if (SUCCESS != get_lastvalue($avail_itemid, ITEM_VALUE_TYPE_UINT64, \$avail_value, \$avail_clock))
+	{
+		fail("cannot get lastvalue for avail item $avail_itemid");
+	}
+
+	my $downtime_value;
+	my $downtime_clock;
+
+	if (SUCCESS != get_lastvalue($downtime_itemid, ITEM_VALUE_TYPE_UINT64, \$downtime_value, \$downtime_clock))
+	{
+		$downtime_value = 0;
+		$downtime_clock = current_month_first_cycle() - 60;
+	}
+
+	if ($downtime_clock >= $avail_clock)
+	{
+		dbg("no new data for nsip '$nsip'");
+		return;
+	}
+
+	my $clock_first = $downtime_clock + 60;
+	my $clock_last = $downtime_clock + (60 * $max_cycles);
+
+	if ($clock_last > $avail_clock)
+	{
+		$clock_last = $avail_clock;
+	}
+
+	my $rows = db_select(
+		"select clock,value".
+		" from history_uint".
+		" where itemid=$avail_itemid".
+			" and " . sql_time_condition($clock_first, $clock_last)
+	);
+
+	fail("cannot obtain values for ns '$nsip' avail item") unless (scalar(@{$rows}));
+
+	foreach my $row (@{$rows})
+	{
+		my $clock = $row->[0];
+		my $avail_value = $row->[1];
+
+		my $prev_clock = $clock - 60;
+		my $month_changed = (month_start($prev_clock) != month_start($clock) ? 1 : 0);
+
+		my $new_downtime_value = ($month_changed ? 0 : $downtime_value) + ($avail_value == DOWN ? 1 : 0);
+
+		push_value($tld, $downtime_key, $clock, $new_downtime_value);
+
+		$downtime_value = $new_downtime_value;
+		$clock += 60;
+	}
+}
